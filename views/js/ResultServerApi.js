3f150647a37c46a18cc00210a89dbc150fa280a3
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-17 15:13:54 +0000

    + qtism 0.87 + resultServerApi
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiTest/trunk@16256 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/ResultServerApi.js
+++ views/js/ResultServerApi.js
@@ -3,9 +3,22 @@ function ResultServerApi(endpoint, testServiceCallId, testDefinition, testCompil
 	this.testServiceCallId = testServiceCallId;
 	this.testDefinition = testDefinition;
 	this.testCompilation = testCompilation;
+	
+	//private variable
+    var qtiRunner = null;
+    this.setQtiRunner = function(runner){
+        qtiRunner = runner;
+    };
+    
+    this.getQtiRunner = function(){
+        return qtiRunner;
+    };
 }
 
 ResultServerApi.prototype.submitItemVariables = function(itemId, serviceCallId, responses, scores, events, callback){
+	
+	var that = this;
+	
 	$.ajax({
 		url  		: this.endpoint + 'storeItemVariableSet?serviceCallId=' + encodeURIComponent(this.testServiceCallId),
 		data 		: {
@@ -18,7 +31,18 @@ ResultServerApi.prototype.submitItemVariables = function(itemId, serviceCallId, 
 		type 		: 'post',
 		dataType	: 'json',
 		success		: function(reply) {
-			callback();
+			if(reply.success){
+                var fbCount = 0;
+                if(reply.itemSession){
+                    var runner = that.getQtiRunner();
+                    if(runner){
+                        fbCount = runner.showFeedbacks(reply.itemSession, callback);
+                    }
+                }
+                if(!fbCount){
+                    callback();
+                }
+            }
 		}
 	});
 };
