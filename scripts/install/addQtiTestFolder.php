3a43afd423256d377862014701c26f728712005e
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-04 16:15:25 +0000

    use new constant files_path
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiTest/trunk@18859 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/install/addQtiTestFolder.php
+++ scripts/install/addQtiTestFolder.php
@@ -23,10 +23,13 @@
 /*
  * This post-installation script creates a fodler for the QTI Tests
  */
-$extension = common_ext_ExtensionsManager::singleton()->getExtensionById('taoQtiTest');
-$dataPath = $extension->getDir(). 'data' . DIRECTORY_SEPARATOR . 'testdata' . DIRECTORY_SEPARATOR;
+$dataPath = FILES_PATH . 'taoQtiTest' . DIRECTORY_SEPARATOR;
+if (file_exists($dataPath)) {
+    helpers_File::emptyDirectory($dataPath);
+}
 
 $source = tao_models_classes_FileSourceService::singleton()->addLocalSource('QTI test datasource', $dataPath);
-$directory = new core_kernel_file_File($source->createFile(''));
+mkdir($dataPath.'testData');
+$directory = new core_kernel_file_File($source->createFile('', 'testData'));
 
 taoQtiTest_models_classes_QtiTestService::singleton()->setQtiTestDirectory($directory);
