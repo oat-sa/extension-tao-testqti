dd49e417e7b0d5b054e5a3f760902a47a1cd596f
Joel Bout <joel@taotesting.com>
2014-07-23 10:19:02 +0200

    removed nolinger used QtiCommon extension from requirements

--- manifest.php
+++ manifest.php
@@ -37,8 +37,7 @@ return array(
 	'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '2.6',
-        'taoQtiItem' => '2.6',
-        'taoQtiCommon' => '2.6'
+        'taoQtiItem' => '2.6'
     ),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOTest.rdf'
