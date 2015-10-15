d68f013cb7a1abe266c9b7c2d33106770c9edd52
bugalood <jerome@taotesting.com>
2015-10-15 10:33:54 +0200

    TAO-1661 Support of assessmentSection->visible attribute in... Test Runner!

--- views/js/controller/runtime/testRunner.js
+++ views/js/controller/runtime/testRunner.js
@@ -685,7 +685,11 @@ define([
             updateContext: function () {
 
                 $controls.$title.text(this.testContext.testTitle);
-                $controls.$position.text(' - ' + this.testContext.sectionTitle);
+                
+                // Visibility of section?
+                var sectionText = (this.testContext.isDeepestSectionVisible === true) ? (' - ' + this.testContext.sectionTitle) : '';
+                
+                $controls.$position.text(sectionText);
                 $controls.$titleGroup.show();
             },
 
