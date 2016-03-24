422755802e62651b5181e9ca2ffe7a18350d8417
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-03-24 16:25:05 +0100

    increase version from 2.21.0 to 2.21.1

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -268,6 +268,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('2.20.0');
         }
 
-        $this->skip('2.20.0','2.21.0');
+        $this->skip('2.20.0','2.21.1');
     }
 }
