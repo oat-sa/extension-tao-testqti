0b6908c0670269197909a3676cc1ef62b38a3390
Aleh Hutnikau <hutnikau@1pt.com>
2016-04-05 15:49:58 +0300

    Add tests of QtiTimeLine class

--- test/runner/time/QtiTimeLineTest.php
+++ test/runner/time/QtiTimeLineTest.php
@@ -19,11 +19,9 @@
 
 namespace oat\taoQtiTest\test\runner\time;
 
-use oat\taoTests\model\runner\time\TimeLine;
-use oat\taoTests\model\runner\time\TimePoint;
-use oat\taoQtiTest\model\runner\time\QtiTimeLine;
+use oat\taoQtiTest\models\runner\time\QtiTimeLine;
+use oat\taoTests\models\runner\time\TimePoint;
 use oat\tao\test\TaoPhpUnitTestRunner;
-
 
 /**
  * Test the {@link QtiTimeLine}
@@ -39,70 +37,330 @@ class QtiTimeLineTest extends TaoPhpUnitTestRunner
     }
 
     /**
-     * Test the timeline instantiation
+     * Test the TimeLine instantiation
      */
     public function testConstructor()
     {
-        $timeline = new QtiTimeLine();
-        $this->assertInstanceOf('oat\taoTests\models\runner\time\TimeLine', $timeline);
+        $timeLine = new QtiTimeLine([
+            new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+            new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+        ]);
+        $this->assertInstanceOf('oat\taoTests\models\runner\time\TimeLine', $timeLine);
     }
 
     /**
-     * Test adding one point to the Timline
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
      */
-    public function testAddPoint()
+    public function testSerialize($points)
     {
-        $timeline = new QtiTimeLine();
-        $this->assertEquals(count($timeline->getPoints()), 0, 'The timeline is empty added');
-
-        $point    = new TimePoint(['test-A', 'item-A'], 1459519570.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER);
-        $result = $timeline->add($point);
-
-        $this->assertInstanceOf('oat\taoTests\models\runner\time\TimeLine', $result);
-        $this->assertSame($result, $timeline);
-
-        $this->assertEquals(count($timeline->getPoints()), 1, 'A point has been added');
+        $timeLine = new QtiTimeLine($points);
+        $timeLineUnserialized = new QtiTimeLine();
+        $data = $timeLine->serialize();
+        $timeLineUnserialized->unserialize($data);
+        $this->assertEquals($timeLine->getPoints(), $timeLineUnserialized->getPoints());
     }
 
     /**
-     * Test adding one point to the Timline
-     *
-     * @dataProvider removePointsProvider
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
      */
-    public function testRemovePoint($flags, $type, $target, $expected)
+    public function testUnserialize($points)
     {
-        $timeline = new QtiTimeLine();
-        $points   = [
-            new TimePoint(['test-a', 'item-a'], 1459519570.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
-            new TimePoint(['test-a', 'item-a'], 1459519590.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
-            new TimePoint(['test-a', 'item-a'], 1459519572.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
-            new TimePoint(['test-a', 'item-a'], 1459519891.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
-            new TimePoint(['test-a', 'item-b'], 1459519870.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
-            new TimePoint(['test-a', 'item-b'], 1459519890.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
-            new TimePoint(['test-a', 'item-b'], 1459519872.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
-            new TimePoint(['test-a', 'item-b'], 1459519891.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT)
-        ];
-        foreach($points as $point){
-            $timeline->add($point);
+        $timeLine = new QtiTimeLine($points);
+        $timeLineUnserialized = new QtiTimeLine();
+        $data = $timeLine->serialize();
+        $timeLineUnserialized->unserialize($data);
+        $this->assertEquals($timeLineUnserialized->getPoints(), $timeLine->getPoints());
+    }
+
+
+    /**
+     * Test the QtiTimeLine::getPoints()
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
+     */
+    public function testGetPoints($points)
+    {
+        $timeLine = new QtiTimeLine();
+        foreach ($points as $point) {
+            $timeLine->add($point);
+        }
+        $this->assertEquals($points, $timeLine->getPoints());
+    }
+
+    /**
+     * Test the QtiTimeLine::getPoints()
+     */
+    public function testAdd()
+    {
+        $timeLine = new QtiTimeLine();
+        $this->assertEquals($timeLine->getPoints(), [], 'The timeline is empty added');
+
+        $point = new TimePoint(['test-A', 'item-A'], 1459519570.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER);
+        $result = $timeLine->add($point);
+        $this->assertEquals($timeLine, $result);
+        $this->assertEquals($timeLine->getPoints(), [$point]);
+
+        $point2 = new TimePoint(['test-A', 'item-B'], 1459519770.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER);
+        $timeLine->add($point2);
+        $this->assertEquals($timeLine->getPoints(), [$point, $point2]);
+    }
+
+    /**
+     * Test the QtiTimeLine::remove()
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
+     */
+    public function testRemove($points)
+    {
+        //remove all points
+        $timeLine = new QtiTimeLine($points);
+        $this->assertEquals($points, $timeLine->getPoints());
+        $removed = $timeLine->remove([], TimePoint::TARGET_ALL);
+        $this->assertEquals([], $timeLine->getPoints());
+        $this->assertEquals(count($points), $removed);
+
+        //remove all points
+        $timeLine = new QtiTimeLine($points);
+        $removed = $timeLine->remove([], TimePoint::TARGET_ALL, TimePoint::TYPE_ALL);
+        $this->assertEquals([], $timeLine->getPoints());
+        $this->assertEquals(count($points), $removed);
+
+        //remove all points
+        $timeLine = new QtiTimeLine($points);
+        $removed = $timeLine->remove([]);
+        $this->assertEquals([], $timeLine->getPoints());
+        $this->assertEquals(count($points), $removed);
+
+        //remove second points
+        $pointToRemove = $points[1];
+        $timeLine = new QtiTimeLine($points);
+        $removed = $timeLine->remove($pointToRemove->getTags(), $pointToRemove->getTarget(), $pointToRemove->getType());
+        $expected = $points;
+        unset($expected[1]);
+        $this->assertEquals(1, $removed);
+        $this->assertEquals($expected, $timeLine->getPoints());
+
+        //remove first four points
+        $timeLine = new QtiTimeLine($points);
+        $removed = $timeLine->remove($points[0]->getTags(), TimePoint::TARGET_ALL, TimePoint::TYPE_ALL);
+        $expected = $points;
+        unset($expected[0], $expected[1], $expected[2], $expected[3]);
+        $this->assertEquals(4, $removed);
+        $this->assertEquals($expected, $timeLine->getPoints());
+    }
+
+    /**
+     * Test the QtiTimeLine::clear()
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
+     */
+    public function testClear($points)
+    {
+        $timeLine = new QtiTimeLine($points);
+        $this->assertEquals($points, $timeLine->getPoints());
+        $result = $timeLine->clear();
+        $this->assertEquals($timeLine, $result);
+        $this->assertEquals([], $timeLine->getPoints());
+    }
+
+    /**
+     * Test the QtiTimeLine::filter()
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
+     */
+    public function testFilter($points)
+    {
+        $timeLine = new QtiTimeLine($points);
+        $filteredTimeLine = $timeLine->filter(null, TimePoint::TARGET_CLIENT);
+        foreach ($filteredTimeLine->getPoints() as $point) {
+            $this->assertEquals(TimePoint::TARGET_CLIENT, $point->getTarget());
         }
 
-        $this->assertEqual(count($timeline->getPoints()), count($points), 'The points are in the timeline');
+        $timeLine = new QtiTimeLine($points);
+        $filteredTimeLine = $timeLine->filter(null, TimePoint::TYPE_ALL, TimePoint::TYPE_START);
+        foreach ($filteredTimeLine->getPoints() as $point) {
+            $this->assertEquals(TimePoint::TYPE_START, $point->getType());
+        }
 
-        $result = $timeline->remove($flags, $type, $target);
-        $this->assertEqual($result, $expected, "The query removed the expected number of points");
-        $this->assertEqual(count($timeline->getPoints()), count($points) - $expected, 'The points are removed from the timeline');
+        $timeLine = new QtiTimeLine($points);
+        $filteredTimeLine = $timeLine->filter(null, TimePoint::TARGET_CLIENT, TimePoint::TYPE_START);
+        foreach ($filteredTimeLine->getPoints() as $point) {
+            $this->assertEquals(TimePoint::TARGET_CLIENT, $point->getTarget());
+            $this->assertEquals(TimePoint::TYPE_START, $point->getType());
+        }
+
+        $timeLine = new QtiTimeLine($points);
+        $filteredTimeLine = $timeLine->filter(['test-a', 'item-a']);
+        foreach ($filteredTimeLine->getPoints() as $point) {
+            $this->assertEquals(['test-a', 'item-a'], $point->getTags());
+        }
     }
 
-    public function removePointsProvider()
+    /**
+     * Test the QtiTimeLine::find()
+     * @dataProvider linearTestPointsProvider
+     * @param TimePoint[] $points
+     */
+    public function testFind($points)
+    {
+        $timeLine = new QtiTimeLine($points);
+
+        $foundPoints = $timeLine->find(null, TimePoint::TARGET_CLIENT);
+        foreach ($foundPoints as $point) {
+            $this->assertEquals(TimePoint::TARGET_CLIENT, $point->getTarget());
+        }
+
+        $foundPoints = $timeLine->find(null, TimePoint::TARGET_ALL, TimePoint::TYPE_START);
+        foreach ($foundPoints as $point) {
+            $this->assertEquals(TimePoint::TYPE_START, $point->getType());
+        }
+
+        $foundPoints = $timeLine->find(null, TimePoint::TARGET_SERVER, TimePoint::TYPE_START);
+        foreach ($foundPoints as $point) {
+            $this->assertEquals(TimePoint::TARGET_SERVER, $point->getTarget());
+            $this->assertEquals(TimePoint::TYPE_START, $point->getType());
+        }
+
+        $foundPoints = $timeLine->find(['test-a', 'item-a']);
+        foreach ($foundPoints as $point) {
+            $this->assertEquals(['test-a', 'item-a'], $point->getTags());
+        }
+    }
+
+    /**
+     * Test the QtiTimeLine::compute()
+     */
+    public function testCompute()
+    {
+        $points = [
+            new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+            new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+            new TimePoint(['test-a', 'item-a'], 1459519510.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
+            new TimePoint(['test-a', 'item-a'], 1459519512.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+        ];
+        $timeLine = new QtiTimeLine($points);
+
+        $duration = $timeLine->compute(['test-a', 'item-a'], TimePoint::TARGET_SERVER);
+        $expectedDuration = $points[3]->getTimestamp() - $points[0]->getTimestamp();
+        $this->assertEquals($duration, $expectedDuration);
+
+        $duration = $timeLine->compute(['test-a', 'item-a'], TimePoint::TARGET_CLIENT);
+        $expectedDuration = $points[2]->getTimestamp() - $points[1]->getTimestamp();
+        $this->assertEquals($duration, $expectedDuration);
+
+        $timeLine = new QtiTimeLine([$points[1], $points[2]]);
+        $duration = $timeLine->compute(null, TimePoint::TARGET_ALL);
+        $expectedDuration = $points[2]->getTimestamp() - $points[1]->getTimestamp();
+        $this->assertEquals($duration, $expectedDuration);
+    }
+
+    /**
+     * Test the QtiTimeLine::compute()
+     * @dataProvider malformedRangeExceptionProvider
+     * @expectedException \oat\taoTests\models\runner\time\MalformedRangeException
+     * @param TimePoint[] $points
+     */
+    public function testComputeMalformedRangeException($points)
+    {
+        $timeLine = new QtiTimeLine($points);
+        $timeLine->compute(['test-a', 'item-a']);
+    }
+
+    /**
+     * Test the QtiTimeLine::compute()
+     * @expectedException \oat\taoTests\models\runner\time\IncompleteRangeException
+     */
+    public function testComputeIncompleteRangeException()
+    {
+        $timeLine = new QtiTimeLine([
+            new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+            new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+            new TimePoint(['test-a', 'item-a'], 1459519510.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
+        ]);
+        $timeLine->compute(['test-a', 'item-a']);
+    }
+
+    /**
+     * Test the QtiTimeLine::compute()
+     * @expectedException \oat\taoTests\models\runner\time\InconsistentRangeException
+     * @dataProvider inconsistentRangeExceptionProvider
+     * @param TimePoint[] $points
+     */
+    public function testInconsistentRangeException($points)
+    {
+        $timeLine = new QtiTimeLine($points);
+        $timeLine->compute(['test-a', 'item-a']);
+    }
+
+    /**
+     * @return array
+     */
+    public function linearTestPointsProvider()
     {
         return [
-            ['test-a', TimePoint::TYPE_ALL, TimePoint::TARGET_ALL, 4],
-            ['item-a', TimePoint::TYPE_ALL, TimePoint::TARGET_ALL, 4],
-            ['item-a', TimePoint::TYPE_START, TimePoint::TARGET_ALL, 2],
-            ['item-a', TimePoint::TYPE_ALL, QtiTimePoint::TARGET_CLIENT, 0, 2],
-            [['item-a', 'item-b'], TimePoint::TYPE_ALL, TimePoint::TARGET_ALL, 8],
-            [['test-a', 'item-b'], TimePoint::TYPE_START, TimePoint::TARGET_SERVER, 1],
-            ['item-c', TimePoint::TYPE_ALL, TimePoint::TARGET_ALL, 0],
+            [
+                'linearTest' => [
+                    //item-a
+                    new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-a'], 1459519510.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-a'], 1459519512.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                    //item-b
+                    new TimePoint(['test-a', 'item-b'], 1459519600.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-b'], 1459519602.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-b'], 1459519610.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-b'], 1459519612.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                    //item-b
+                    new TimePoint(['test-a', 'item-c'], 1459519700.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-c'], 1459519702.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-c'], 1459519710.2422, TimePoint::TYPE_END, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-c'], 1459519712.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function malformedRangeExceptionProvider()
+    {
+        return [
+            [
+                [
+                    new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                ],
+            ],
+            [
+                [
+                    new TimePoint(['test-a', 'item-a'], 1459519500.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function inconsistentRangeExceptionProvider()
+    {
+        return [
+            [
+                [
+                    new TimePoint(['test-a', 'item-a'], 1459519502.2422, TimePoint::TYPE_START, TimePoint::TARGET_CLIENT),
+                    new TimePoint(['test-a', 'item-a'], 1459519510.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                ]
+            ],
+            [
+                [
+                    new TimePoint(['test-a', 'item-a'], 1459519522.2422, TimePoint::TYPE_START, TimePoint::TARGET_SERVER),
+                    new TimePoint(['test-a', 'item-a'], 1459519510.2422, TimePoint::TYPE_END, TimePoint::TARGET_SERVER),
+                ]
+            ],
         ];
     }
 }
