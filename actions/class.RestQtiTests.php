1ad226e04ee1672807a1bbe4813efd7d49ed2bd2
Absar <sgilani@pcgus.com>
2016-04-18 00:41:26 -0500

    REST API FOR QTI TEST

---
+++ actions/class.RestQtiTests.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/**
+ *
+ * @author Absar Gilani & Rashid - PCG Team - {absar.gilani6@gmail.com}
+
+ */
+class taoQtiTest_actions_RestQtiTests extends tao_actions_CommonRestModule {
+ 	
+	 const QTI_TESTPACKAGE = 'qtiTestContent' ;
+	 
+	 const ITEMSLIST = 'itemsList';
+	
+
+	public function __construct()
+	{
+	    parent::__construct();
+		//The service that implements or inherits get/getAll/getRootClass ... for that particular type of resources
+		$this->service = taoQtiTest_models_classes_CrudQtiTestsService::singleton();
+	}
+
+	/**
+	 * Optionnaly a specific rest controller may declare
+	 * aliases for parameters used for the rest communication
+	 */
+	protected function getParametersAliases()
+	{
+	    return array_merge(parent::getParametersAliases(), array(
+		"qtiPackage" => self::QTI_TESTPACKAGE,
+		"itemsList"  => self::ITEMSLIST
+		   
+	    ));
+	}
+	
+	
+	protected function importQtiPackage($parameters){
+		$report=null;
+		if(isset($_FILES["qtiPackage"]))
+		{
+			if($_FILES["qtiPackage"]["name"]) {
+				$filename = $_FILES["qtiPackage"]["name"];
+				$source = $_FILES["qtiPackage"]["tmp_name"];
+				$type = $_FILES["qtiPackage"]["type"];
+
+				$name = explode(".", $filename);
+				$accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
+				foreach ($accepted_types as $mime_type) {
+					if ($mime_type == $type) {
+						$okay = true;
+						break;
+					}
+				}
+				$continue = strtolower($name[1]) == 'zip' ? true : false;
+				if (!$continue) {
+					throw new common_exception_InvalidArgumentType();
+				}
+				$path = dirname(__FILE__) . '/';  // absolute path to the directory where zipper.php is in
+				$filenoext = basename($filename, '.zip');  // absolute path to the directory where zipper.php is in (lowercase)
+				$filenoext = basename($filenoext, '.ZIP');  // absolute path to the directory where zipper.php is in (when uppercase)
+				$targetdir = $path . $filenoext; // target directory
+				$targetzip = $path . $filename; // target zip file
+				/* create directory if not exists', otherwise overwrite */
+				/* target directory is same as filename without extension */
+				if (is_dir($targetdir)) rmdir_recursive($targetdir);
+				if(move_uploaded_file($source, $targetzip)) {
+					$report = $this->service->importFromArray($parameters, $targetzip);
+				}
+			}else{
+				throw new common_exception_InvalidArgumentType();
+			}
+		}
+		return $report;
+	}
+	/**
+	 * Optionnal Requirements for parameters to be sent on every service
+	 *
+	 */
+	protected function getParametersRequirements()
+	{
+	    return array(
+		/** you may use either the alias or the uri, if the parameter identifier
+		 *  is set it will become mandatory for the method/operation in $key
+		* Default Parameters Requirents are applied
+		* type by default is not required and the root class type is applied
+         *    @example :"post"=> array("login", "password")
+		*/
+		"post"=> array("qtiPackage"),
+		"itemsList"=> self::ITEMSLIST	
+	    );
+	}
+	
+	/**
+	 *
+	 * @author Rashid and Absar -PCG Team
+	 */	 
+	protected function post() {
+		try {
+			$parameters = $this->getParameters();
+			/**
+			 * This code snippet  import QTI Package or create new item
+			 *
+			 * @author Rashid Mumtaz & Absar - PCG Team - {absar.gilani6@gmail.com & rashid.mumtaz372@gmail.com}
+			 * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+			 * @package taoItems
+			 *
+			 *
+			 */
+				if(isset($parameters[self::QTI_TESTPACKAGE])){
+					// Import QTI Package			
+					$data = $this->importQtiPackage($parameters);
+					foreach ($data as $r) {
+						$values = $r->getData();
+						$testid = $values->rdfsResource->getUri();
+							foreach ($values->items as $item) 			 
+								$itemsid[] = $item->getUri();                                          			
+					}		
+					$data = array('testId' => $testid, 'testItems' => $itemsid);
+				}
+			else{
+				// Create new Item
+				$data = $this->service->createFromArray($parameters);			
+			}
+			
+		} catch (Exception $e) {
+			return $this->returnFailure($e);
+		}
+		return $this->returnSuccess($data);
+	}
+}
---
+++ models/classes/class.CrudQtiTestsService.php
@@ -0,0 +1,92 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA
+ * 
+ */
+
+/**
+ * Crud services implements basic CRUD services, orginally intended for 
+ * REST controllers/ HTTP exception handlers . 
+ * 
+ * Consequently the signatures and behaviors is closer to REST and throwing HTTP like exceptions
+ * 
+ * @author Absar Gilani, absar.gilani@gmail.com
+ *   
+ */
+ 
+ //use \oat\taoQtiTest\models\classes\testqti;
+class taoQtiTest_models_classes_CrudQtiTestsService
+    extends tao_models_classes_CrudService
+{
+
+	/** (non-PHPdoc)
+	 * @see tao_models_classes_CrudSservice::getClassService()
+	 */
+	protected function getClassService() {
+		return taoQtiTest_models_classes_QtiTestService::singleton();
+	}
+
+    /**
+     * (non-PHPdoc)
+     * @see tao_models_classes_CrudService::delete()
+     */    
+    public function delete( $resource){
+        $this->getClassService()->deleteTest(new core_kernel_classes_Resource($resource));
+        return true;
+    }
+
+    /**
+     * 
+     * @author Rashid Mumtaz & Absar - PCG Team - {absar.gilani6@gmail.com & rashid.mumtaz372@gmail.com}
+     * @param array $propertiesValues
+     * @return core_kernel_classes_Resource
+     */
+    public function createFromArray(array $propertiesValues){
+	
+		if (!isset($propertiesValues[RDFS_LABEL])) {
+			$propertiesValues[RDFS_LABEL] = "";
+		}
+		$type = isset($propertiesValues[RDF_TYPE]) ? $propertiesValues[RDF_TYPE] : $this->getRootClass();
+		$label = $propertiesValues[RDFS_LABEL];
+		unset($propertiesValues[RDFS_LABEL]);
+		unset($propertiesValues[RDF_TYPE]);
+		$resource =  parent::createListedTest($label, $type, $propertiesValues);
+		return $resource;
+    }
+
+    
+
+
+	public function importFromArray(array $propertiesValues,$uploadedFile){
+
+		//test versioning
+		try {
+		      //The zip extraction is a long process that can exceed the 30s timeout
+                helpers_TimeOutHelper::setTimeOutLimit(helpers_TimeOutHelper::LONG);
+                $class = new core_kernel_classes_Class(TAO_TEST_CLASS);
+                $report = taoQtiTest_models_classes_QtiTestService::singleton()->importMultipleTests($class, $uploadedFile);               
+                helpers_TimeOutHelper::reset();
+                tao_helpers_File::remove($uploadedFile);
+                return $report;
+        }
+        catch (Exception $e) {
+            return common_report_Report::createFailure($e->getMessage());
+        }
+	}
+}
+
+?>
