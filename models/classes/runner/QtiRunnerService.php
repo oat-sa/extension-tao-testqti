96b489c045e32c4903c99485169810d93516a9f1
Aleh Hutnikau <hutnikau@1pt.com>
2016-06-23 12:58:43 +0300

    trigger TestTimeoutEvent after changing rote position

--- models/classes/runner/QtiRunnerService.php
+++ models/classes/runner/QtiRunnerService.php
@@ -1069,7 +1069,7 @@ class QtiRunnerService extends ConfigurableService implements RunnerService
         /* @var TestSession $session */
         $session = $context->getTestSession();
 
-        $event = new TestTimeoutEvent($session, $timeOutException->getCode());
+        $event = new TestTimeoutEvent($session, $timeOutException->getCode(), true);
         $this->getServiceManager()->get(EventManager::CONFIG_ID)->trigger($event);
 
         $isLinear = $session->getCurrentNavigationMode() === NavigationMode::LINEAR;
@@ -1109,6 +1109,9 @@ class QtiRunnerService extends ConfigurableService implements RunnerService
                 }
                 break;
         }
+
+        $event = new TestTimeoutEvent($session, $timeOutException->getCode(), false);
+        $this->getServiceManager()->get(EventManager::CONFIG_ID)->trigger($event);
 
         $this->continueInteraction($context);
     }
