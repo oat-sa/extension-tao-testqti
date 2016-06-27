032fa6da79b13d19e479d5ec41130286121afa7a
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-06-27 10:50:48 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -33,11 +33,11 @@ return array(
     'label' => 'QTI test model',
 	'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '2.36.0',
+    'version' => '2.37.0',
 	'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=2.19.0',
-        'taoQtiItem' => '>=2.33'
+        'taoQtiItem' => '>=2.34.0'
     ),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOTest.rdf'
