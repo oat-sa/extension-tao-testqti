67ea65528eded39c6457d81b3aa4cbfa5d37ac23
= <jerome@taotesting.com>
2016-08-11 14:40:43 +0200

    Bump version.

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -531,7 +531,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             ];
             $extension->setConfig('testRunner', $config);
 
-            $this->setVersion('5.4.0');
+            $this->setVersion('5.5.0');
         }
     }
 }
