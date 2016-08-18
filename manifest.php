d040b472cca3c7c9761831d2ebdd2fc180d50c16
= <jerome@taotesting.com>
2016-08-18 15:28:39 +0200

    Now require taoQtiItem 4.1.

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return array(
 	'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.0.0',
-        'taoQtiItem' => '>=3.9',
+        'taoQtiItem' => '>=4.1',
         'tao'        => '>=6.0.0'
     ),
 	'models' => array(
