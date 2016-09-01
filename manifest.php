ed76c8d8c4e91adec8759c622852912c66efb596
jsconan <jsconan@gmail.com>
2016-09-01 08:24:05 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -33,12 +33,12 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '5.8.3',
+    'version' => '5.9.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.0.0',
         'taoQtiItem' => '>=4.2',
-        'tao'        => '>=6.0.0'
+        'tao'        => '>=7.6.0'
     ),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOTest.rdf'
