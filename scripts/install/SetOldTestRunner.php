971e3934aa56aab3136301d500b6430b5fd46af1
jsconan <jsconan@gmail.com>
2016-09-20 10:49:51 +0200

    Add script to setup the old TR

--- scripts/install/SetOldTestRunner.php
+++ scripts/install/SetOldTestRunner.php
@@ -2,28 +2,28 @@
 namespace oat\taoQtiTest\scripts\install;
 
 /**
- * Class SetNewTestRunner
+ * Class SetOldTestRunner
  * 
- * Setup the new Test Runner
+ * Setup the old Test Runner
  * 
  * @package oat\taoQtiTest\scripts\install
  */
-class SetNewTestRunner extends \common_ext_action_InstallAction
+class SetOldTestRunner extends \common_ext_action_InstallAction
 {
     public function __invoke($params)
     {
         $deliveryExt = \common_ext_ExtensionsManager::singleton()->getExtensionById('taoDelivery');
         $deliveryServerConfig = $deliveryExt->getConfig('deliveryServer');
-        $deliveryServerConfig->setOption('deliveryContainer', 'oat\\taoDelivery\\helper\\container\\DeliveryClientContainer');
+        $deliveryServerConfig->setOption('deliveryContainer', 'oat\\taoDelivery\\helper\\container\\DeliveryClientService');
         $deliveryExt->setConfig('deliveryServer', $deliveryServerConfig);
 
         $itemQtiExt = \common_ext_ExtensionsManager::singleton()->getExtensionById('taoQtiItem');
-        $compilerClassConfig = 'oat\\taoQtiItem\\model\\QtiJsonItemCompiler';
+        $compilerClassConfig = 'oat\\taoQtiItem\\model\\QtiItemCompiler';
         $itemQtiExt->setConfig('compilerClass', $compilerClassConfig);
 
         $testQtiExt = \common_ext_ExtensionsManager::singleton()->getExtensionById('taoQtiTest');
         $testRunnerConfig = $testQtiExt->getConfig('testRunner');
-        $testRunnerConfig['test-session'] = 'oat\\taoQtiTest\\models\\runner\\session\\TestSession';
+        $testRunnerConfig['test-session'] = '\\taoQtiTest_helpers_TestSession';
         $testQtiExt->setConfig('testRunner', $testRunnerConfig);
 
         return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'New test runner activated');
