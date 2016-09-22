660816cfc921958d25adbb0fcb630ffe34e56bda
= <jerome@taotesting.com>
2016-09-22 15:34:59 +0200

    Improve performance of test import.

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -608,6 +608,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('5.12.0');
         }
 
-        $this->skip('5.12.0', '5.16.0');
+        $this->skip('5.12.0', '5.16.1');
     }
 }
