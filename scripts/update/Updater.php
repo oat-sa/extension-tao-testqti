1dc4bb447146824cd661ad3586f1afebc3bfb6e4
jsconan <jsconan@gmail.com>
2016-10-14 10:49:53 +0200

    Add the documentViewer plugin

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -647,5 +647,19 @@ class Updater extends \common_ext_ExtensionUpdater {
         }
 
         $this->skip('5.18.0', '5.25.1');
+
+        if ($this->isVersion('5.25.1')) {
+            $registry = PluginRegistry::getRegistry();
+            $registry->register(TestPlugin::fromArray([
+                'id' => 'documentViewer',
+                'name' => 'Document Viewer',
+                'module' => 'taoQtiTest/runner/plugins/tools/documentViewer/documentViewer',
+                'description' => 'Display a document as requested by an event',
+                'category' => 'tools',
+                'active' => false,
+                'tags' => []
+            ]));
+            $this->setVersion('5.26.0');
+        }
     }
 }
