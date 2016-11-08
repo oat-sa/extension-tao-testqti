24047fe57948a93ddba947a1b9da0e243eeb4bd9
Christophe Noël <christophe@taotesting.com>
2016-11-08 18:25:59 +0100

    setup tests for highlighter

---
+++ views/js/test/runner/plugins/tools/highlighter/highlighter/test.js
@@ -0,0 +1,39 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'lodash',
+    'helpers',
+    'taoTests/runner/runner',
+    'taoQtiTest/test/runner/mocks/providerMock',
+    'taoQtiTest/runner/plugins/tools/highlighter/highlighter'
+], function(_, helpers, runnerFactory, providerMock, highlighterFactory) {
+    'use strict';
+
+    QUnit.module('highlighterFactory');
+
+    QUnit.test('module', function(assert) {
+        assert.ok(typeof highlighterFactory === 'function', 'the module expose a function');
+    });
+
+
+
+
+});
