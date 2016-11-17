66cadc29c4c0b22439c7a8d87aafbdddb2063d99
Sam Sipasseuth <sam@taotesting.com>
2016-11-17 16:50:21 +0100

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -794,6 +794,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('5.34.0');
         }
 
-        $this->skip('5.34.0', '5.35.0');
+        $this->skip('5.34.0', '5.35.1');
     }
 }
