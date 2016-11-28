3884a7859810fd2eab22a14e9067b3c6c0d77df0
Aleh Hutnikau <hutnikau@1pt.com>
2016-11-28 15:40:08 +0300

    Trigger removetimer event

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '5.41.0',
+    'version' => '5.41.1',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
