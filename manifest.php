7a075a8d504dfa7a5197b59d510743dc61ab961b
jsconan <jsconan@gmail.com>
2016-12-14 09:10:56 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -34,12 +34,12 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '5.47.0',
+    'version' => '5.48.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
-        'taoQtiItem' => '>=5.14.0',
-        'tao'        => '>=7.39.0'
+        'taoQtiItem' => '>=6.7.0',
+        'tao'        => '>=7.40.0'
     ),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOTest.rdf'
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -939,5 +939,17 @@ class Updater extends \common_ext_ExtensionUpdater {
 
             $this->setVersion('5.47.0');
         }
+        
+        if ($this->isVersion('5.47.0')) {
+            $extension = \common_ext_ExtensionsManager::singleton()->getExtensionById('taoQtiTest');
+
+            $config = $extension->getConfig('testRunner');
+
+            $config['test-taker-review-use-title'] = true;
+
+            $extension->setConfig('testRunner', $config);
+
+            $this->setVersion('5.48.0');
+        }
     }
 }
