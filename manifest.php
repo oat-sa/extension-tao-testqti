b7e1c7f3370aeecb03343665b67d3224e4b2fd63
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-02-02 17:42:25 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -34,11 +34,11 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '5.58.3',
+    'version' => '5.59.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
-        'taoQtiItem' => '>=6.8.0',
+        'taoQtiItem' => '>=6.12.0',
         'tao'        => '>=7.42.0'
     ),
 	'models' => array(
