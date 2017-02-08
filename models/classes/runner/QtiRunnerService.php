6553df9adc4c98f133f7d617d562e5a0112aae67
Aleh Hutnikau <hutnikau@1pt.com>
2017-02-08 15:47:13 +0300

    finish delivery if exit button pressed

--- models/classes/runner/QtiRunnerService.php
+++ models/classes/runner/QtiRunnerService.php
@@ -889,6 +889,7 @@ class QtiRunnerService extends ConfigurableService implements RunnerService
             $this->getServiceManager()->get(EventManager::CONFIG_ID)->trigger($event);
 
             $session->endTestSession();
+            $this->finish($context);
         } else {
             throw new \common_exception_InvalidArgumentType(
                 'QtiRunnerService',
