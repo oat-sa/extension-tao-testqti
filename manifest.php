5bd52970664929b0729689a6277b89132f08c6e3
Christophe Noël <christophe@taotesting.com>
2017-02-10 11:47:56 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '6.1.4',
+    'version' => '6.2.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1034,7 +1034,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             ]));
             $this->setVersion('6.1.0');
         }
-        
-        $this->skip('6.1.0', '6.1.4');
+
+        $this->skip('6.1.0', '6.2.0');
     }
 }
