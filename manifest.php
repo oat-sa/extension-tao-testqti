a0602c8868df1d13d9f04eefbf1ed642e2150a40
Mikhail Kamarouski <Komarouski@1pt.com>
2017-03-02 15:56:41 +0300

    prevented failure when required metadata is missing

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '6.7.1',
+    'version' => '6.7.2',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
