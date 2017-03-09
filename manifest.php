bf7f358785b7300105677c5bb59104b1bd98951e
cmoyon <camille@taotesting.com>
2017-03-09 10:31:28 +0100

    Bump to version 6.12.0

--- manifest.php
+++ manifest.php
@@ -34,11 +34,11 @@ return array(
     'label' => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license' => 'GPL-2.0',
-    'version' => '6.11.1',
+    'version' => '6.12.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoTests' => '>=3.7.0',
-        'taoQtiItem' => '>=6.16.0',
+        'taoQtiItem' => '>=6.18.0',
         'tao'        => '>=7.76.0'
     ),
 	'models' => array(
