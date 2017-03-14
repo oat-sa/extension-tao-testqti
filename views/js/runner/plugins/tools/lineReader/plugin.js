a010adac4ad9e76937a69702e99238ac1a440cad
Christophe Noël <christophe@taotesting.com>
2017-03-14 11:20:53 +0100

    duplicated plugin and tests

---
+++ views/js/runner/plugins/tools/lineReader/plugin.js
@@ -0,0 +1,168 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * Test Runner Tool Plugin : Line Reader
+ *
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'i18n',
+    'taoTests/runner/plugin',
+    'ui/hider',
+    'util/shortcut',
+    'util/namespace',
+    'taoQtiTest/runner/plugins/tools/lineReader/compoundMask'
+], function ($, _, __, pluginFactory, hider, shortcut, namespaceHelper, compoundMaskFactory) {
+    'use strict';
+
+    /**
+     * The public name of the plugin
+     * @type {String}
+     */
+    var pluginName = 'line-reader';
+
+    /**
+     * The prefix of actions triggered through the event loop
+     * @type {String}
+     */
+    var actionPrefix = 'tool-' + pluginName + '-';
+
+
+    /**
+     * Returns the configured plugin
+     */
+    return pluginFactory({
+
+        name: pluginName,
+
+        /**
+         * Initialize the plugin (called during runner's init)
+         */
+        init: function init() {
+            var self = this;
+
+            var testRunner = this.getTestRunner();
+            var testData = testRunner.getTestData() || {};
+            var testConfig = testData.config || {};
+            var pluginShortcuts = (testConfig.shortcuts || {})[this.getName()] || {};
+
+            var compoundMask = compoundMaskFactory();
+
+            // create buttons
+            this.button = this.getAreaBroker().getToolbox().createEntry({
+                title: __('Line Reader'),
+                icon: 'insert-horizontal-line',
+                control: 'line-reader',
+                text: __('Line Reader')
+            });
+
+            // attach user events
+            this.button
+                .on('click', function(e) {
+                    e.preventDefault();
+                    testRunner.trigger(actionPrefix + 'toggle');
+                });
+
+            if (testConfig.allowShortcuts) {
+                if (pluginShortcuts.toggle) {
+                    shortcut.add(namespaceHelper.namespaceAll(pluginShortcuts.toggle, this.getName(), true), function () {
+                        testRunner.trigger(actionPrefix + 'toggle');
+                    }, { avoidInput: true, prevent: true });
+                }
+            }
+
+            //start disabled
+            this.disable();
+
+            /**
+             * Checks if the plugin is currently available
+             * @returns {Boolean}
+             */
+            function isEnabled() {
+                var context = testRunner.getTestContext() || {},
+                    options = context.options || {};
+                //to be activated with the special category x-tao-option-lineReader
+                return !!options.lineReader;
+            }
+
+            /**
+             * Is plugin activated ? if not, then we hide the plugin
+             */
+            function togglePlugin() {
+                if (isEnabled()) {
+                    self.show();
+                } else {
+                    self.hide();
+                }
+            }
+
+            //update plugin state based on changes
+            testRunner
+                .on('loaditem', togglePlugin)
+                .on('enabletools renderitem', function () {
+                    self.enable();
+                })
+                .on('disabletools unloaditem', function () {
+                    self.disable();
+                })
+                .on(actionPrefix + 'toggle', function () {
+                    if (isEnabled()) {
+                        compoundMask.toggle();
+                    }
+                });
+        },
+
+        /**
+         * Called during the runner's destroy phase
+         */
+        destroy: function destroy() {
+            shortcut.remove('.' + this.getName());
+        },
+
+        /**
+         * Enable the button
+         */
+        enable: function enable() {
+            this.button.enable();
+        },
+
+        /**
+         * Disable the button
+         */
+        disable: function disable() {
+            this.button.disable();
+        },
+
+        /**
+         * Show the button
+         */
+        show: function show() {
+            this.button.show();
+        },
+
+        /**
+         * Hide the button
+         */
+        hide: function hide() {
+            this.button.hide();
+        }
+    });
+});
---
+++ views/js/test/runner/plugins/tools/lineReader/plugin/test.js
@@ -0,0 +1,290 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'lodash',
+    'helpers',
+    'ui/hider',
+    'taoTests/runner/runner',
+    'taoQtiTest/test/runner/mocks/providerMock',
+    'taoQtiTest/runner/plugins/tools/lineReader/plugin'
+], function(_, helpers, hider, runnerFactory, providerMock, pluginFactory) {
+    'use strict';
+
+    var pluginApi;
+    var providerName = 'mock';
+    runnerFactory.registerProvider(providerName, providerMock());
+
+    /**
+     * The following tests applies to all plugins
+     */
+    QUnit.module('pluginFactory');
+
+    QUnit.test('module', 3, function(assert) {
+        var runner = runnerFactory(providerName);
+
+        assert.equal(typeof pluginFactory, 'function', "The pluginFactory module exposes a function");
+        assert.equal(typeof pluginFactory(runner), 'object', "The plugin factory produces an instance");
+        assert.notStrictEqual(pluginFactory(runner), pluginFactory(runner), "The plugin factory provides a different instance on each call");
+    });
+
+
+    pluginApi = [
+        { name : 'init', title : 'init' },
+        { name : 'render', title : 'render' },
+        { name : 'finish', title : 'finish' },
+        { name : 'destroy', title : 'destroy' },
+        { name : 'trigger', title : 'trigger' },
+        { name : 'getTestRunner', title : 'getTestRunner' },
+        { name : 'getAreaBroker', title : 'getAreaBroker' },
+        { name : 'getConfig', title : 'getConfig' },
+        { name : 'setConfig', title : 'setConfig' },
+        { name : 'getState', title : 'getState' },
+        { name : 'setState', title : 'setState' },
+        { name : 'show', title : 'show' },
+        { name : 'hide', title : 'hide' },
+        { name : 'enable', title : 'enable' },
+        { name : 'disable', title : 'disable' }
+    ];
+
+    QUnit
+        .cases(pluginApi)
+        .test('plugin API ', 1, function(data, assert) {
+            var runner = runnerFactory(providerName);
+            var timer = pluginFactory(runner);
+            assert.equal(typeof timer[data.name], 'function', 'The pluginFactory instances expose a "' + data.name + '" function');
+        });
+
+
+    QUnit.asyncTest('pluginFactory.init', function(assert) {
+        var runner = runnerFactory(providerName);
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        plugin.init()
+            .then(function() {
+                assert.equal(plugin.getState('init'), true, 'The plugin is initialised');
+
+                QUnit.start();
+            })
+            .catch(function(err) {
+                assert.ok(false, 'The init failed: ' + err);
+                QUnit.start();
+            });
+    });
+
+
+    /**
+     * The following tests applies to buttons-type plugins
+     */
+    QUnit.module('plugin button');
+
+    QUnit.asyncTest('render/destroy button', function(assert) {
+        var runner = runnerFactory(providerName);
+        var areaBroker = runner.getAreaBroker();
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        QUnit.expect(4);
+
+        plugin.init()
+            .then(function() {
+                var $container = runner.getAreaBroker().getToolboxArea(),
+                    $button;
+
+                areaBroker.getToolbox().render($container);
+
+                $button = $container.find('[data-control="line-reader"]');
+
+                assert.equal($button.length, 1, 'The trigger button has been inserted');
+                assert.equal($button.hasClass('disabled'), true, 'The trigger button has been rendered disabled');
+                assert.equal($button.hasClass('disabled'), true, 'The remove button has been rendered disabled');
+
+                areaBroker.getToolbox().destroy();
+
+                $button = $container.find('[data-control="line-reader"]');
+
+                assert.equal($button.length, 0, 'The trigger button has been removed');
+                QUnit.start();
+
+            })
+            .catch(function(err) {
+                assert.ok(false, 'Error in init method: ' + err);
+                QUnit.start();
+            });
+    });
+
+
+    QUnit.asyncTest('enable/disable button', function(assert) {
+        var runner = runnerFactory(providerName);
+        var areaBroker = runner.getAreaBroker();
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        QUnit.expect(2);
+
+        plugin.init()
+            .then(function() {
+                var $container = runner.getAreaBroker().getToolboxArea(),
+                    $button;
+
+                areaBroker.getToolbox().render($container);
+
+                plugin.enable()
+                    .then(function() {
+                        $button = $container.find('[data-control="line-reader"]');
+
+                        assert.equal($button.hasClass('disabled'), false, 'The trigger button has been enabled');
+
+                        plugin.disable()
+                            .then(function() {
+                                assert.equal($button.hasClass('disabled'), true, 'The trigger button has been disabled');
+
+                                QUnit.start();
+                            })
+                            .catch(function(err) {
+                                assert.ok(false, 'error in disable method: ' + err);
+                                QUnit.start();
+                            });
+                    })
+                    .catch(function(err) {
+                        assert.ok(false, 'error in enable method: ' + err);
+                        QUnit.start();
+                    });
+            })
+            .catch(function(err) {
+                assert.ok(false, 'Error in init method: ' + err);
+                QUnit.start();
+            });
+    });
+
+
+    QUnit.asyncTest('show/hide button', function(assert) {
+        var runner = runnerFactory(providerName);
+        var areaBroker = runner.getAreaBroker();
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        QUnit.expect(2);
+
+        plugin.init()
+            .then(function() {
+                var $container = runner.getAreaBroker().getToolboxArea(),
+                    $button;
+
+                areaBroker.getToolbox().render($container);
+
+                plugin.hide()
+                    .then(function() {
+                        $button = $container.find('[data-control="line-reader"]');
+
+                        assert.ok(hider.isHidden($button), 'The trigger button has been hidden');
+
+                        plugin.show()
+                            .then(function() {
+                                assert.ok(! hider.isHidden($button), 'The trigger button is visible');
+
+                                QUnit.start();
+                            })
+                            .catch(function(err) {
+                                assert.ok(false, 'error in disable method: ' + err);
+                                QUnit.start();
+                            });
+                    })
+                    .catch(function(err) {
+                        assert.ok(false, 'error in enable method: ' + err);
+                        QUnit.start();
+                    });
+            })
+            .catch(function(err) {
+                assert.ok(false, 'Error in init method: ' + err);
+                QUnit.start();
+            });
+    });
+
+
+    QUnit.asyncTest('runner events: loaditem / unloaditem', function(assert) {
+        var runner = runnerFactory(providerName);
+        var areaBroker = runner.getAreaBroker();
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        QUnit.expect(3);
+
+        runner.setTestContext({
+            options: {
+                highlighter: true
+            }
+        });
+
+        plugin.init()
+            .then(function() {
+                var $container = runner.getAreaBroker().getToolboxArea(),
+                    $button;
+
+                areaBroker.getToolbox().render($container);
+
+                $button = $container.find('[data-control="line-reader"]');
+
+                runner.trigger('loaditem');
+
+                assert.ok(! hider.isHidden($button), 'The trigger button is visible');
+
+                runner.trigger('unloaditem');
+
+                assert.ok(! hider.isHidden($button), 'The trigger button is still visible');
+
+                assert.equal($button.hasClass('disabled'), true, 'The trigger button has been disabled');
+
+                QUnit.start();
+            })
+            .catch(function(err) {
+                assert.ok(false, 'Error in init method: ' + err);
+                QUnit.start();
+            });
+    });
+
+
+    QUnit.asyncTest('runner events: renderitem', function(assert) {
+        var runner = runnerFactory(providerName);
+        var areaBroker = runner.getAreaBroker();
+        var plugin = pluginFactory(runner, runner.getAreaBroker());
+
+        QUnit.expect(2);
+
+        plugin.init()
+            .then(function() {
+                var $container = runner.getAreaBroker().getToolboxArea(),
+                    $button;
+
+                areaBroker.getToolbox().render($container);
+
+                $button = $container.find('[data-control="line-reader"]');
+
+                runner.trigger('renderitem');
+
+                assert.ok(! hider.isHidden($button), 'The trigger button is visible');
+
+                assert.equal($button.hasClass('disabled'), false, 'The trigger button is not disabled');
+
+                QUnit.start();
+            })
+            .catch(function(err) {
+                assert.ok(false, 'Error in init method: ' + err);
+                QUnit.start();
+            });
+    });
+
+});
