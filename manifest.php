d58605594f37a23ca58823fb1ce0086eb24f1079
jsconan <jsconan@gmail.com>
2017-03-17 12:52:26 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '6.17.0',
+    'version'     => '6.17.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoTests'   => '>=3.7.0',
