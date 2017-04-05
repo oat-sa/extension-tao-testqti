2e08e7235dfdf6276701dc5ded9f14aa738a99d9
Aleh Hutnikau <hutnikau@1pt.com>
2017-04-05 15:07:07 +0300

    Add reason to pause request

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1152,6 +1152,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion(('6.18.0'));
         }
 
-        $this->skip('6.18.0', '7.2.0');
+        $this->skip('6.18.0', '7.2.1');
     }
 }
