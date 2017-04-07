44bff1a6719d6aca706b920d4048056e70977a34
Christophe Noël <christophe@taotesting.com>
2017-04-07 10:47:40 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '7.2.1',
+    'version'     => '7.3.0',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoTests'   => '>=4.0.0',
