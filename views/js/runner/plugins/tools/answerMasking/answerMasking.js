90be679f965f0675efe6b49939f3900cf3b56642
Christophe Noël <christophe@taotesting.com>
2017-04-25 17:58:06 +0200

    functionnal component barebone

---
+++ views/js/runner/plugins/tools/answerMasking/answerMasking.js
@@ -0,0 +1,70 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ */
+/**
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'jquery',
+    'core/statifier',
+    'ui/component'
+], function($, statifier, componentFactory) {
+    'use strict';
+
+
+
+    return function answerMaskingFactory($contentArea) {
+        var answerMasking,
+            masks;
+
+        answerMasking = {
+            enable: function enable() {
+                var $qtiChoices = $contentArea.find('.qti-choice');
+
+                $qtiChoices.css({
+                    'background-color': 'red'
+                });
+                $qtiChoices.each(function () {
+                    $(this).prepend($('<span>', {
+                        style: 'background-color: yellow',
+                        html: 'mask'
+                    }));
+                });
+
+
+                this.setState('enabled', true);
+
+
+
+            },
+
+            disable: function disable() {
+                var $qtiChoices = $contentArea.find('.qti-choice');
+
+                $qtiChoices.css({
+                    'background-color': 'white'
+                });
+
+                this.setState('enabled', false);
+            }
+        };
+
+        statifier(answerMasking);
+
+        return answerMasking;
+    };
+});
