eb841fefa922d0fa4f5b8ea0924bfcb502fc58a4
Joel Bout <joel@taotesting.com>
2017-05-03 11:13:22 +0200

    Use ACL proxy instead of implementation

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -44,6 +44,7 @@ use oat\oatbox\filesystem\FileSystemService;
 use oat\taoQtiTest\models\files\QtiFlysystemFileManager;
 use oat\tao\model\import\ImportersService;
 use oat\taoQtiTest\models\import\QtiTestImporter;
+use oat\tao\model\accessControl\func\AclProxy;
 
 /**
  *
@@ -258,9 +259,11 @@ class Updater extends \common_ext_ExtensionUpdater {
         $this->setVersion($currentVersion);
 
         if ($this->isBetween('2.16.0','2.17.0')) {
-            $proctorRole = new \core_kernel_classes_Resource('http://www.tao.lu/Ontologies/TAO.rdf#DeliveryRole');
-            $accessService = \funcAcl_models_classes_AccessService::singleton();
-            $accessService->grantModuleAccess($proctorRole, 'taoQtiTest', 'Runner');
+            AclProxy::applyRule(new AccessRule(
+                'grant',
+                'http://www.tao.lu/Ontologies/TAO.rdf#DeliveryRole',
+                ['ext' => 'taoQtiTest' , 'mod' => 'Runner']
+            ));
 
             try {
                 $this->getServiceManager()->get(QtiRunnerService::CONFIG_ID);
@@ -1207,6 +1210,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('8.1.0');
         }
 
-        $this->skip('8.1.0', '9.0.0');
+        $this->skip('8.1.0', '9.0.1');
     }
 }
