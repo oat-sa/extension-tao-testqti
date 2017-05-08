f98746b3c09faec369aa25753549203ac4eeff78
Christophe Noël <christophe@taotesting.com>
2017-05-08 18:33:54 +0200

    completed list of categories

--- models/classes/TestCategoryProvider.php
+++ models/classes/TestCategoryProvider.php
@@ -44,6 +44,111 @@ class TestCategoryProvider extends ConfigurableService
                 'qtiCategory'   => 'x-tao-option-nextPartWarning',
                 'description'   => __('displays a warning before the user finishes the part'),
                 'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'nextSectionWarning',
+                'label'         => __('Next Section Warning'),
+                'qtiCategory'   => 'x-tao-option-nextSectionWarning',
+                'description'   => __('displays a next section button that warns the user that they will not be able to return to the section'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'nextSection',
+                'label'         => __('Next Section Button'),
+                'qtiCategory'   => 'x-tao-option-nextSection',
+                'description'   => __('displays a next section button'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'unansweredWarning',
+                'label'         => __('Unanswered Warning'),
+                'qtiCategory'   => 'x-tao-option-unansweredWarning',
+                'description'   => __('displays a warning before the user finishes the part, only if there are unanswered/marked for review items'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'noExitTimedSectionWarning',
+                'label'         => __('noExitTimedSectionWarning'),
+                'qtiCategory'   => 'x-tao-option-noExitTimedSectionWarning',
+                'description'   => __('disable the warning automatically displayed upon exiting a timed section'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'exit',
+                'label'         => __('Exit Button'),
+                'qtiCategory'   => 'x-tao-option-exit',
+                'description'   => __('displays an exit button'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'markReview',
+                'label'         => __('Mark For Review'),
+                'qtiCategory'   => 'x-tao-option-markReview',
+                'description'   => __('displays a mark for review button. This option requires requires the x-tao-option-reviewScreen option'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'reviewScreen',
+                'label'         => __('Review Screen'),
+                'qtiCategory'   => 'x-tao-option-reviewScreen',
+                'description'   => __('displays the review screen / navigator'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'calculator',
+                'label'         => __('Calculator'),
+                'qtiCategory'   => 'x-tao-option-calculator',
+                'description'   => __('Enable calculator'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'answerMasking',
+                'label'         => __('Answer Masking'),
+                'qtiCategory'   => 'x-tao-option-answerMasking',
+                'description'   => __('enable answer masking tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'areaMasking',
+                'label'         => __('Area Masking'),
+                'qtiCategory'   => 'x-tao-option-areaMasking',
+                'description'   => __('enable area masking tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'highlighter',
+                'label'         => __('Highlighter'),
+                'qtiCategory'   => 'x-tao-option-highlighter',
+                'description'   => __('enable highlighter tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'lineReader',
+                'label'         => __('Line Reader'),
+                'qtiCategory'   => 'x-tao-option-lineReader',
+                'description'   => __('enable line reader tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'magnifier',
+                'label'         => __('Magnifier'),
+                'qtiCategory'   => 'x-tao-option-magnifier',
+                'description'   => __('enable magnifier tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'eliminator',
+                'label'         => __('Answer Eliminator'),
+                'qtiCategory'   => 'x-tao-option-eliminator',
+                'description'   => __('enable eliminator tool'),
+                'order'         => 50
+            ]),
+            TestCategory::fromArray([
+                'id'            => 'informational',
+                'label'         => __('Informational item'),
+                'qtiCategory'   => 'x-tao-itemusage-informational',
+                'description'   => __('describe the item as informational'),
+                'order'         => 50
             ])
         ];
     }
