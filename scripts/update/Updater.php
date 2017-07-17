3ffecf51c83e6fbdd9a22ab16b4029cb2c33303d
Mikhail Kamarouski <kamarouski@1pt.com>
2017-07-17 13:39:12 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1321,6 +1321,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('10.1.0');
         }
 
-        $this->skip('10.1.0', '10.1.2');
+        $this->skip('10.1.0', '11.0.0');
     }
 }
