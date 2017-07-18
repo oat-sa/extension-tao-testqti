f4fa832266bcb01df03656389bfd39784618737e
antoinerobin <antoine.robin.2014@utt.fr>
2017-07-18 16:35:01 +0200

    update version number

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1321,6 +1321,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('10.1.0');
         }
 
-        $this->skip('10.1.0', '10.2.2');
+        $this->skip('10.1.0', '10.3.0');
     }
 }
