866a43303e24779849a810a49e307450dbe3a12e
siwane <camille@taotesting.com>
2017-07-19 08:50:03 +0000

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1321,6 +1321,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('10.1.0');
         }
 
-        $this->skip('10.1.0', '10.3.0');
+        $this->skip('10.1.0', '10.4.0');
     }
 }
