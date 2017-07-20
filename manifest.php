033498e5cf649fc7ebabaca37bae88440667aaa1
bugalot <jerome@taotesting.com>
2017-07-20 09:21:51 +0200

    Bump to version 10.4.1

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '10.4.0',
+    'version'     => '10.4.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoTests'   => '>=6.3.0',
