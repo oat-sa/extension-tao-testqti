8a6862484844f0f5a7cddc3e8fc21849f7c25ee8
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-08-04 13:39:42 +0300

    Small code style fixes

--- models/classes/cat/AdaptiveSectionInjectionException.php
+++ models/classes/cat/AdaptiveSectionInjectionException.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
@@ -26,7 +26,7 @@ use \Exception;
  * This Exception class must be thrown in reaction to an error occuring
  * during a adaptive section injection process.
  *
- * @author Aleksej Tikhanovich <jerome@taotesting.com>
+ * @author Aleksej Tikhanovich <aleksej@taotesting.com>
  *
  */
 class AdaptiveSectionInjectionException extends Exception
