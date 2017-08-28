b9fba664cf34c9d3906506c9a23e356907666de6
siwane <camille@taotesting.com>
2017-08-28 08:17:43 +0000

    Refactor import service

--- models/classes/import/class.TestImport.php
+++ models/classes/import/class.TestImport.php
@@ -1,21 +1,21 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
- * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- * 
+ *
+ * Copyright (c) 2013-2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
  */
 
 use oat\oatbox\PhpSerializable;
@@ -29,56 +29,60 @@ use oat\tao\model\upload\UploadService;
  * @access public
  * @author Joel Bout, <joel@taotesting.com>
  * @package taoQTI
- 
  */
 class taoQtiTest_models_classes_import_TestImport implements tao_models_classes_import_ImportHandler, PhpSerializable
 {
     use PhpSerializeStateless;
+
     /**
      * (non-PHPdoc)
+     *
      * @see tao_models_classes_import_ImportHandler::getLabel()
      */
-    public function getLabel() {
-    	return __('QTI/APIP Test Content Package');
-    }
-    
-    /**
-     * (non-PHPdoc)
-     * @see tao_models_classes_import_ImportHandler::getForm()
-     */
-    public function getForm() {
-    	$form = new taoQtiTest_models_classes_import_TestImportForm();
-    	return $form->getForm();
+    public function getLabel()
+    {
+        return __('QTI/APIP Test Content Package');
     }
 
     /**
      * (non-PHPdoc)
+     *
+     * @see tao_models_classes_import_ImportHandler::getForm()
+     */
+    public function getForm()
+    {
+        $form = new taoQtiTest_models_classes_import_TestImportForm();
+        return $form->getForm();
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
      * @see tao_models_classes_import_ImportHandler::import()
      */
-    public function import($class, $form) {
-		
+    public function import($class, $form)
+    {
         try {
             $fileInfo = $form->getValue('source');
 
-            if(isset($fileInfo['uploaded_file'])){
+            if (isset($fileInfo['uploaded_file'])) {
 
                 /** @var  UploadService $uploadService */
                 $uploadService = ServiceManager::getServiceManager()->get(UploadService::SERVICE_ID);
-                $uploadedFile = $uploadService->getUploadedFile($fileInfo['uploaded_file']);
+                $uploadedFile = $uploadService->getUploadedFlyFile($fileInfo['uploaded_file']);
 
                 // The zip extraction is a long process that can exceed the 30s timeout
                 helpers_TimeOutHelper::setTimeOutLimit(helpers_TimeOutHelper::LONG);
-                
+
                 $report = taoQtiTest_models_classes_QtiTestService::singleton()->importMultipleTests($class, $uploadedFile);
-                
+
                 helpers_TimeOutHelper::reset();
-                $uploadService->remove($uploadService->getUploadedFlyFile($fileInfo['uploaded_file']));
+                $uploadService->remove($uploadedFile);
             } else {
                 throw new common_exception_Error('No source file for import');
             }
             return $report;
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             return common_report_Report::createFailure($e->getMessage());
         }
     }
