bbcfe5ee938eda16b6cb85fea5c453b7d81a5f10
eric <eacraft13@gmail.com>
2017-08-30 16:02:32 +0200

    Bump patch version (again)
    tao-4963

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1575,6 +1575,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('13.2.0');
         }
 
-        $this->skip('13.2.0', '13.2.1');
+        $this->skip('13.2.0', '13.2.2');
     }
 }
