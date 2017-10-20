fdd4157fe5046cb06f149baa35255f293d8f73f8
jsconan <jsconan@gmail.com>
2017-10-20 16:39:53 +0200

    Add storage manager

---
+++ models/classes/runner/StorageManager.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ *
+ */
+
+namespace oat\taoQtiTest\models\runner;
+
+use oat\oatbox\service\ConfigurableService;
+use oat\tao\model\state\StateStorage;
+
+/**
+ * Class StorageManager
+ * 
+ * Manage the storage in order to centralize its access.
+ * The reading of data can be done at any time, the first call will put the data in memory cache.
+ * Each change will only update the cache, and mark it to be processed upon persisting.
+ * The actual writing should be done once, at the end of the request, by invoking the `persist()` method.
+ * 
+ * @package oat\taoQtiTest\models\classes\runner
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+class StorageManager extends ConfigurableService
+{
+    const SERVICE_ID = 'taoQtiTest/StorageManager';
+
+    /**
+     * The data does not exist in the storage
+     */
+    const STATE_NOT_FOUND = -1;
+
+    /**
+     * The data is aligned with the storage
+     */
+    const STATE_ALIGNED = 0;
+
+    /**
+     * The data is pending write to the storage
+     */
+    const STATE_PENDING_WRITE = 1;
+
+    /**
+     * The data is pending delete from the storage
+     */
+    const STATE_PENDING_DELETE = 2;
+
+    /**
+     * Link to the actual storage adapter
+     * @var StateStorage
+     */
+    protected $storage;
+
+    /**
+     * In memory cache for read/pending data
+     * @var array
+     */
+    protected $cache = [];
+
+    /**
+     * Gets a key that will be used to cache data.
+     *
+     * @param string $userId
+     * @param string $callId
+     * @return string
+     */
+    protected function getCacheKey($userId, $callId)
+    {
+        return $userId . '/' . $callId;
+    }
+
+    /**
+     * Puts data in the cache. Maintain the link to the userId/callId pair.
+     * Also keep the dirty state that will be used when persisting the data to the actual storage.
+     *
+     * @param string $key
+     * @param string $userId
+     * @param string $callId
+     * @param string $data
+     * @param int $state
+     */
+    protected function putInCache($key, $userId, $callId, $data, $state = self::STATE_ALIGNED)
+    {
+        $this->cache[$key] = [
+            'userId' => $userId,
+            'callId' => $callId,
+            'state' => $state,
+            'data' => $data
+        ];
+    }
+
+    /**
+     * Checks if a dataset exists for the provided key.
+     *
+     * @param string $key
+     * @return bool
+     */
+    protected function exists($key)
+    {
+        return isset($this->cache[$key]) && in_array($this->cache[$key]['state'], [self::STATE_ALIGNED, self::STATE_PENDING_WRITE]);
+    }
+
+    /**
+     * Gets a dataset from the cache.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    protected function getFromCache($key)
+    {
+        if ($this->exists($key)) {
+            return $this->cache[$key]['data'];
+        }
+        return null;
+    }
+
+    /**
+     * @return StateStorage
+     */
+    public function getStorage()
+    {
+        if (!$this->storage) {
+            $this->storage = $this->getServiceLocator()->get(StateStorage::SERVICE_ID);
+        }
+        return $this->storage;
+    }
+
+    /**
+     * @param StateStorage $storage
+     * @return StorageManager
+     */
+    public function setStorage(StateStorage $storage)
+    {
+        $this->storage = $storage;
+        return $this;
+    }
+
+    /**
+     * Applies a dataset to be stored.
+     *
+     * @param string $userId
+     * @param string $callId
+     * @param string $data
+     * @return boolean
+     */
+    public function set($userId, $callId, $data)
+    {
+        $key = $this->getCacheKey($userId, $callId);
+        $cache = $this->getFromCache($key);
+        if (is_null($cache) || $cache != $data) {
+            $this->putInCache($key, $userId, $callId, $data, self::STATE_PENDING_WRITE);
+        }
+        return true;
+    }
+
+    /**
+     * Gets a dataset from the store using the provided keys.
+     * Will return null if the dataset doesn't exist.
+     *
+     * @param string $userId
+     * @param string $callId
+     * @return string
+     */
+    public function get($userId, $callId)
+    {
+        $key = $this->getCacheKey($userId, $callId);
+        if (!isset($this->cache[$key])) {
+            $data = $this->getStorage()->get($userId, $callId);
+            $state = is_null($data) ? self::STATE_NOT_FOUND : self::STATE_ALIGNED;
+            $this->putInCache($key, $userId, $callId, $data, $state);
+        }
+
+        return $this->getFromCache($key);
+    }
+
+    /**
+     * Whenever or not a dataset exists.
+     *
+     * @param string $userId
+     * @param string $callId
+     * @return boolean
+     */
+    public function has($userId, $callId)
+    {
+        $key = $this->getCacheKey($userId, $callId);
+        if (!isset($this->cache[$key])) {
+            return $this->getStorage()->has($userId, $callId);
+        }
+        return $this->exists($key);
+    }
+
+    /**
+     * Marks the the dataset to be removed from the storage.
+     *
+     * @param string $userId
+     * @param string $callId
+     * @return boolean
+     */
+    public function del($userId, $callId)
+    {
+        $key = $this->getCacheKey($userId, $callId);
+        $this->putInCache($key, $userId, $callId, null, self::STATE_PENDING_DELETE);
+        return true;
+    }
+
+    /**
+     * Sends the changes to the storage.
+     *
+     * @return bool
+     */
+    public function persist()
+    {
+        $success = true;
+        $storage = $this->getStorage();
+        foreach ($this->cache as $cache) {
+            switch ($cache['state']) {
+                case self::STATE_PENDING_WRITE:
+                    if (!$storage->set($cache['userId'], $cache['callId'], $cache['data'])) {
+                        $success = false;
+                    }
+                    break;
+
+                case self::STATE_PENDING_DELETE:
+                    if (!$storage->del($cache['userId'], $cache['callId'])) {
+                        $success = false;
+                    }
+                    break;
+            }
+        }
+        return $success;
+    }
+}
---
+++ test/runner/StorageManagerTest.php
@@ -0,0 +1,324 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ *
+ */
+
+namespace oat\taoQtiTest\test\runner;
+
+use oat\tao\model\state\StateStorage;
+use oat\tao\test\TaoPhpUnitTestRunner;
+use oat\taoQtiTest\models\runner\StorageManager;
+use Prophecy\Argument;
+use Prophecy\Prophet;
+
+/**
+ * Class StorageManagerTest
+ * @package oat\taoQtiTest\test\runner
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+class StorageManagerTest extends TaoPhpUnitTestRunner
+{
+    /**
+     * @throws \common_ext_ExtensionException
+     */
+    public function setUp()
+    {
+        \common_ext_ExtensionsManager::singleton()->getExtensionById('taoQtiTest');
+    }
+
+    /**
+     * Tests the storage component of StorageManager
+     */
+    public function testStorage()
+    {
+        $prophet = new Prophet();
+        $mockStorageService = $prophet->prophesize(StateStorage::class)->reveal();
+        $mockStorage = $prophet->prophesize(StateStorage::class)->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorageService
+        ]));
+
+        $this->assertInstanceOf(StateStorage::class, $storageManager->getStorage());
+        $this->assertEquals($mockStorageService, $storageManager->getStorage());
+
+        $storageManager->setStorage($mockStorage);
+
+        $this->assertEquals($mockStorage, $storageManager->getStorage());
+    }
+
+    /**
+     * Test the StorageManager::get() method
+     */
+    public function testGet()
+    {
+        $cachedData = 'this is a test';
+        $userId = 'user123';
+        $callId = '456';
+        $buffer = [
+            $userId => [
+                $callId => $cachedData
+            ]
+        ];
+
+        $prophet = new Prophet();
+        $prophecy = $prophet->prophesize(StateStorage::class);
+        $prophecy->get(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            if (isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]])) {
+                return $buffer[$args[0]][$args[1]];
+            }
+            return null;
+        });
+        $mockStorage = $prophecy->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorage
+        ]));
+
+        $this->assertEquals($cachedData, $storageManager->get($userId, $callId));
+        $this->assertEquals(null, $storageManager->get('foo', $callId));
+        $this->assertEquals(null, $storageManager->get($userId, 'foo'));
+        $this->assertEquals(null, $storageManager->get('foo', 'foo'));
+    }
+
+    /**
+     * Test the StorageManager::set() method
+     */
+    public function testSet()
+    {
+        $data1 = 'this is a test';
+        $data2 = 'this is another test';
+        $userId = 'user123';
+        $callId = '456';
+
+        $prophet = new Prophet();
+        $prophecy = $prophet->prophesize(StateStorage::class);
+        $prophecy->get(Argument::type('string'), Argument::type('string'))->willReturn(null);
+        $prophecy->set(Argument::type('string'), Argument::type('string'), Argument::type('string'))->willThrow(new \Exception('Set() should not be called!'));
+        $mockStorage = $prophecy->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorage
+        ]));
+
+        $this->assertEquals(null, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(true, $storageManager->set($userId, $callId, $data1));
+        $this->assertEquals($data1, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(true, $storageManager->set($userId, $callId, $data2));
+        $this->assertEquals($data2, $storageManager->get($userId, $callId));
+    }
+
+    /**
+     * Test the StorageManager::has() method
+     */
+    public function testHas()
+    {
+        $data1 = 'this is a test';
+        $data2 = 'this is another test';
+        $userId = 'user123';
+        $callId = '456';
+        $buffer = [
+            $userId => [
+                $callId => $data1
+            ]
+        ];
+
+        $prophet = new Prophet();
+        $prophecy = $prophet->prophesize(StateStorage::class);
+        $prophecy->get(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            if (isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]])) {
+                return $buffer[$args[0]][$args[1]];
+            }
+            return null;
+        });
+        $prophecy->has(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            return isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]]);
+        });
+        $prophecy->set(Argument::type('string'), Argument::type('string'), Argument::type('string'))->willThrow(new \Exception('Set() should not be called!'));
+        $mockStorage = $prophecy->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorage
+        ]));
+
+        $this->assertEquals(true, $storageManager->has($userId, $callId));
+        $this->assertEquals($data1, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(false, $storageManager->has('foo', 'bar'));
+        $this->assertEquals(null, $storageManager->get('foo', 'bar'));
+
+        $this->assertEquals(true, $storageManager->set('foo', 'bar', $data2));
+        $this->assertEquals(true, $storageManager->has('foo', 'bar'));
+        $this->assertEquals($data2, $storageManager->get('foo', 'bar'));
+    }
+
+    /**
+     * Test the StorageManager::del() method
+     */
+    public function testDel()
+    {
+        $data1 = 'this is a test';
+        $data2 = 'this is another test';
+        $userId = 'user123';
+        $callId = '456';
+        $buffer = [
+            $userId => [
+                $callId => $data1
+            ]
+        ];
+
+        $prophet = new Prophet();
+        $prophecy = $prophet->prophesize(StateStorage::class);
+        $prophecy->get(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            if (isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]])) {
+                return $buffer[$args[0]][$args[1]];
+            }
+            return null;
+        });
+        $prophecy->has(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            return isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]]);
+        });
+        $prophecy->set(Argument::type('string'), Argument::type('string'), Argument::type('string'))->willThrow(new \Exception('Set() should not be called!'));
+        $prophecy->del(Argument::type('string'), Argument::type('string'))->willThrow(new \Exception('Del() should not be called!'));
+
+        $mockStorage = $prophecy->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorage
+        ]));
+
+        $this->assertEquals(true, $storageManager->has($userId, $callId));
+        $this->assertEquals($data1, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(true, $storageManager->del($userId, $callId));
+        $this->assertEquals(false, $storageManager->has($userId, $callId));
+        $this->assertEquals(null, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(null, $storageManager->get('foo', 'bar'));
+        $this->assertEquals(true, $storageManager->set('foo', 'bar', $data2));
+        $this->assertEquals(true, $storageManager->has('foo', 'bar'));
+        $this->assertEquals($data2, $storageManager->get('foo', 'bar'));
+
+        $this->assertEquals(true, $storageManager->del('foo', 'bar'));
+        $this->assertEquals(false, $storageManager->has('foo', 'bar'));
+        $this->assertEquals(null, $storageManager->get('foo', 'bar'));
+    }
+
+    /**
+     * Test the StorageManager::persist() method
+     */
+    public function testPersist()
+    {
+        $data1 = 'this is a test';
+        $data2 = 'this is another test';
+        $userId = 'user123';
+        $callId = '456';
+        $buffer = [
+            $userId => [
+                $callId => $data1
+            ]
+        ];
+
+        $expectedBuffer0 = $buffer;
+
+        $expectedBuffer1 = [
+            $userId => [
+                $callId => $data2
+            ],
+            'foo' => [
+                'bar' => 'foo bar'
+            ]
+        ];
+
+        $expectedBuffer2 = [
+            $userId => [
+                $callId => $data2
+            ]
+        ];
+
+        $prophet = new Prophet();
+        $prophecy = $prophet->prophesize(StateStorage::class);
+        $prophecy->get(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            if (isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]])) {
+                return $buffer[$args[0]][$args[1]];
+            }
+            return null;
+        });
+        $prophecy->set(Argument::type('string'), Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            $buffer[$args[0]][$args[1]] = $args[2];
+            return true;
+        });
+        $prophecy->has(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            return isset($buffer[$args[0]]) && isset($buffer[$args[0]][$args[1]]);
+        });
+        $prophecy->del(Argument::type('string'), Argument::type('string'))->will(function ($args) use (&$buffer) {
+            if (isset($buffer[$args[0]])) {
+                if (isset($buffer[$args[0]][$args[1]])) {
+                    unset($buffer[$args[0]][$args[1]]);
+                }
+                if (empty($buffer[$args[0]])) {
+                    unset($buffer[$args[0]]);
+                }
+            }
+            return true;
+        });
+
+        $mockStorage = $prophecy->reveal();
+
+        $storageManager = new StorageManager([]);
+        $storageManager->setServiceLocator($this->getServiceManagerProphecy([
+            StateStorage::SERVICE_ID => $mockStorage
+        ]));
+
+
+        $this->assertEquals(true, $storageManager->has($userId, $callId));
+        $this->assertEquals($data1, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(true, $storageManager->set($userId, $callId, $data2));
+        $this->assertEquals($data2, $storageManager->get($userId, $callId));
+
+        $this->assertEquals(false, $storageManager->has('foo', 'bar'));
+        $this->assertEquals(null, $storageManager->get('foo', 'bar'));
+        $this->assertEquals(true, $storageManager->set('foo', 'bar', 'foo bar'));
+        $this->assertEquals(true, $storageManager->has('foo', 'bar'));
+        $this->assertEquals('foo bar', $storageManager->get('foo', 'bar'));
+
+        $this->assertEquals($expectedBuffer0, $buffer);
+
+        $this->assertEquals(true, $storageManager->persist());
+
+        $this->assertEquals($expectedBuffer1, $buffer);
+
+        $this->assertEquals(true, $storageManager->del('foo', 'bar'));
+        $this->assertEquals(false, $storageManager->has('foo', 'bar'));
+        $this->assertEquals(null, $storageManager->get('foo', 'bar'));
+
+        $this->assertEquals($expectedBuffer1, $buffer);
+
+        $this->assertEquals(true, $storageManager->persist());
+
+        $this->assertEquals($expectedBuffer2, $buffer);
+    }
+}
