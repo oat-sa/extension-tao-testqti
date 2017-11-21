ab77859c1b53fb4a7aa65e9c03394a433c613d23
Ionut Padurariu <ionut@taotesting.com>
2017-11-21 16:28:12 +0100

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1704,6 +1704,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('17.17.0');
         }
       
-        $this->skip('17.17.0', '17.17.1');
+        $this->skip('17.17.0', '17.17.2');
     }
 }
