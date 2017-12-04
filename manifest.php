cc0d90aea6a3f57e95a872746144a30e1299cb1f
Vilmos "Chilly" Kovacs <vilmos@taotesting.com>
2017-12-04 10:34:35 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '17.21.7',
+    'version'     => '17.21.8',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoTests'   => '>=6.7.0',
