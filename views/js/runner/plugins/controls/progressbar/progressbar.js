26c950924e2e622247a4ce5d284b7c28fb1714d0
Alexander Zagovorychev <zagovorichev@gmail.com>
2017-12-22 15:58:58 +0300

    Added new mode for the progressbar of the testRunner "questions"

--- views/js/runner/plugins/controls/progressbar/progressbar.js
+++ views/js/runner/plugins/controls/progressbar/progressbar.js
@@ -26,9 +26,10 @@ define([
     'lodash',
     'i18n',
     'taoTests/runner/plugin',
+    'taoQtiTest/runner/helpers/map',
     'tpl!taoQtiTest/runner/plugins/controls/progressbar/progressbar',
     'ui/progressbar'
-], function ($, _, __, pluginFactory, progressTpl){
+], function ($, _, __, pluginFactory, mapHelper, progressTpl){
     'use strict';
 
     /**
@@ -142,6 +143,103 @@ define([
                         __('Item %d of %d', currentPosition, total) :
                         __('Item %d', currentPosition)
                 };
+            },
+
+            /**
+             * Updates the progress bar displaying the questions only
+             * ( for example informational items won't be counted and will be without progress bar)
+             * @param {Object} testContext The progression context
+             * @returns {{ratio: number, label: string}}
+             */
+            questions : function position() {
+
+                //get the current test part in the map
+                var getTestPart = function getTestPart(){
+                    if(testMap && testMap.parts){
+                        return testMap.parts[testContext.testPartId];
+                    }
+                };
+
+                //get the current test section in the map
+                var getTestSection = function getTestSection() {
+                    var testPart = getTestPart();
+                    if(testPart && testPart.sections){
+                        return testPart.sections[testContext.sectionId];
+                    }
+                };
+
+                //provides you the methods to get total and position by scope
+                var progressScopeCounter = {
+                    test : {
+                        total : function(){
+                            if (testMap && testMap.parts) {
+                                return _.reduce(testMap.parts, function (accPart, part) {
+                                    return _.reduce(part.sections, function (accSection, section) {
+                                        return accSection + mapHelper.computeItemStats(section.items).questions;
+                                    }, accPart);
+                                }, 0);
+                            }
+                            return 0;
+                        },
+                        position : function(){
+                            if (testMap && testMap.parts) {
+                                return _.reduce(testMap.parts, function (accPart, part) {
+                                    return _.reduce(part.sections, function (accSection, section) {
+                                        return accSection + mapHelper.computeItemStats(section.items).questionsViewed;
+                                    }, accPart);
+                                }, 0);
+                            }
+                            return 0;
+                        }
+                    },
+                    testPart : {
+                        total : function(){
+                            var testPart = getTestPart();
+                            if(testPart){
+                                return _.reduce(testMap.parts[testContext.testPartId].sections, function(acc, section){
+                                    return acc + mapHelper.computeItemStats(section.items).questions;
+                                }, 0);
+                            }
+                            return 0;
+                        },
+                        position : function(){
+                            var testPart = getTestPart();
+                            if(testPart){
+                                return _.reduce(testMap.parts[testContext.testPartId].sections, function(acc, section){
+                                    return acc + mapHelper.computeItemStats(section.items).questionsViewed;
+                                }, 0);
+                            }
+                            return 0;
+                        }
+                    },
+                    testSection : {
+                        total : function(){
+                            var testSection = getTestSection();
+                            if(testSection){
+                                return mapHelper.computeItemStats(testSection.items).questions;
+                            }
+                            return 0;
+                        },
+                        position : function(){
+                            var testSection = getTestSection();
+                            if(testSection && testSection.items[testContext.itemIdentifier]){
+                                return mapHelper.computeItemStats(testSection.items).questionsViewed;
+                            }
+                            return 0;
+                        }
+                    }
+                };
+
+                var counter = progressScopeCounter[progressConfig.scope] || progressScopeCounter.test;
+                var total = counter.total();
+                var currentPosition = counter.position();
+
+                return {
+                    ratio : total > 0 ? Math.floor(currentPosition / total * 100) : 0,
+                    label : progressConfig.showTotal ?
+                        __('Item %d of %d', currentPosition, total) :
+                        __('Item %d', currentPosition)
+                };
             }
         };
         return updater[progressConfig.indicator]();
@@ -164,6 +262,7 @@ define([
             var testRunner = this.getTestRunner();
             var testData   = testRunner.getTestData();
             var config     = testData.config.progressIndicator || {};
+            var self       = this;
 
             var progressConfig = {
                 indicator: config.type || 'percentage',
@@ -175,10 +274,18 @@ define([
              * Update the progress bar
              */
             var update = function update (){
-                var progressData = progressUpdater(testRunner.getTestContext(), testRunner.getTestMap(), progressConfig);
-                if(progressData && $progressLabel && $progressControl){
-                    $progressLabel.text(progressData.label);
-                    $progressControl.progressbar('value', progressData.ratio);
+                var testContext = testRunner.getTestContext();
+                var testMap = testRunner.getTestMap();
+                var progressData = progressUpdater(testContext, testMap, progressConfig);
+                var item = mapHelper.getItemAt(testMap, testContext.itemPosition);
+                if (item && item.informational) {
+                    self.$element.hide();
+                } else {
+                    self.$element.show();
+                    if (progressData && $progressLabel && $progressControl) {
+                        $progressLabel.text(progressData.label);
+                        $progressControl.progressbar('value', progressData.ratio);
+                    }
                 }
             };
 
