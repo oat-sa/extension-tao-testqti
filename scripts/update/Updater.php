616f615a72c24336c510d9c6d74b66e71342c19c
Ionut Padurariu <ionut@taotesting.com>
2018-01-22 12:14:15 +0100

    bump to big versions

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1762,6 +1762,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('18.9.5');
         }
 
-        $this->skip('18.9.5', '18.10.0');
+        $this->skip('18.9.5', '19.0.0');
     }
 }
