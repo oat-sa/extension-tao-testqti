584bb26f007104403fddb1efe0ac5f67ed9b2782
jsconan <jsconan@gmail.com>
2018-05-09 17:32:03 +0200

    Add component that embed the previewer

---
+++ views/js/previewer/item.js
@@ -0,0 +1,65 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'taoQtiTest/previewer/runner',
+    'taoQtiTest/previewer/provider/item',
+    'taoQtiTest/previewer/plugins/itemPlugins',
+    'css!taoQtiTestCss/item-previewer'
+], function ($, _, previewerFactory, itemProvider, itemPluginsLoader) {
+    'use strict';
+
+    /**
+     * Builds a test runner to preview test item
+     * @param {Object}   config - The testRunner options
+     * @param {String}   config.provider - The provider to use
+     * @param {Object[]} [config.plugins] - A collection of plugins to load
+     * @param {Object[]} [config.providers] - A collection of providers to load.
+     *                                        Will be filtered to only the 'previewer' category.
+     * @param {Boolean} [config.replace] - When the component is appended to its container, clears the place before
+     * @param {Number|String} [config.width] - The width in pixels, or 'auto' to use the container's width
+     * @param {Number|String} [config.height] - The height in pixels, or 'auto' to use the container's height
+     * @param {jQuery|HTMLElement|String} [container] - The container in which renders the component
+     * @returns {previewer}
+     */
+    return function itemPreviewerFactory(config, container) {
+        var itemPlugins = itemPluginsLoader();
+
+        config = config || {};
+        config.loadedPlugins = config.loadedPlugins || {};
+        _.forEach(itemPlugins, function(plugins, category) {
+            config.loadedPlugins[category] = (config.loadedPlugins[category] || []).concat(plugins);
+        });
+
+        config.loadProviders = config.loadProviders || {};
+        config.loadProviders.previewer = [itemProvider];
+        config.provider = config.provider || itemProvider.name;
+
+        return previewerFactory(container || $(document.body), config)
+            .on('ready', function(runner) {
+                var self = this;
+                runner.on('destroy', function() {
+                    self.destroy();
+                });
+            });
+    };
+});
---
+++ views/js/test/previewer/item/test.js
@@ -0,0 +1,233 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'core/promise',
+    'taoQtiTest/previewer/item',
+    'json!taoQtiItem/test/samples/json/space-shuttle.json',
+    'lib/jquery.mockjax/jquery.mockjax',
+    'css!taoQtiTestCss/item-previewer'
+], function ($, _, Promise, previewerFactory, itemData) {
+    'use strict';
+
+    QUnit.module('API');
+
+
+    // prevent the AJAX mocks to pollute the logs
+    $.mockjaxSettings.logger = null;
+    $.mockjaxSettings.responseTime = 1;
+
+    // restore AJAX method after each test
+    QUnit.testDone(function () {
+        $.mockjax.clear();
+    });
+
+
+    QUnit.asyncTest('module', function (assert) {
+        var config = {
+            serviceCallId: 'foo'
+        };
+
+        var previewer1 = previewerFactory(config, $('#fixture-1'));
+        var previewer2 = previewerFactory(config, $('#fixture-2'));
+
+        QUnit.expect(4);
+        $.mockjax({
+            url: '/*',
+            responseText: {
+                success: true
+            }
+        });
+        assert.equal(typeof previewerFactory, 'function', "The previewer module exposes a function");
+        assert.equal(typeof previewer1, 'object', "The previewer factory returns an object");
+        assert.equal(typeof previewer2, 'object', "The previewer factory returns an object");
+        assert.notEqual(previewer1, previewer2, "The previewer factory returns a different instance on each call");
+
+        Promise.all([
+            new Promise(function(resolve) {
+                previewer1.on('ready', function(runner) {
+                    runner.destroy();
+                });
+                previewer1.on('destroy', resolve);
+            }),
+            new Promise(function(resolve) {
+                previewer2.on('ready', function(runner) {
+                    runner.destroy();
+                });
+                previewer2.on('destroy', resolve);
+            })
+        ]).catch(function(err) {
+            console.error(err);
+        }).then(function() {
+            QUnit.start();
+        });
+    });
+
+
+    QUnit.cases([{
+        title: 'itemData in init',
+        fixture: '#fixture-item-1',
+        mock: {
+            url: '/init*',
+            responseText: {
+                success: true,
+                itemIdentifier: 'item-1',
+                itemData: {
+                    content: {
+                        type: 'qti',
+                        data: itemData
+                    },
+                    baseUrl: '',
+                    state: {}
+                }
+            }
+        }
+    }, {
+        title: 'itemRef in init',
+        fixture: '#fixture-item-2',
+        mock: [{
+            url: '/init*',
+            responseText: {
+                success: true,
+                itemIdentifier: 'item-2'
+            }
+        }, {
+            url: '/getItem*',
+            responseText: {
+                success: true,
+                content: {
+                    type: 'qti',
+                    data: itemData
+                },
+                baseUrl: '',
+                state: {}
+            }
+        }]
+    }, {
+        title: 'manual load',
+        fixture: '#fixture-item-3',
+        itemIdentifier: 'item-3',
+        mock: [{
+            url: '/init*',
+            responseText: {
+                success: true
+            }
+        }, {
+            url: '/getItem*',
+            responseText: {
+                success: true,
+                content: {
+                    type: 'qti',
+                    data: itemData
+                },
+                baseUrl: '',
+                state: {}
+            }
+        }]
+    }]).asyncTest('render item ', function (data, assert) {
+        var $container = $(data.fixture);
+        var serviceCallId = 'previewer';
+        var config = {
+            serviceCallId: serviceCallId
+        };
+
+        QUnit.expect(1);
+
+        $.mockjax(data.mock);
+
+        previewerFactory(config, $container)
+            .on('error', function (err) {
+                console.error(err);
+                assert.ok(false, 'An error has occurred');
+                QUnit.start();
+            })
+            .on('ready', function (runner) {
+                runner
+                    .after('renderitem', function () {
+                        assert.ok(true, 'The previewer has been rendered');
+                        runner.destroy();
+                    });
+
+                if (data.itemIdentifier) {
+                    runner.loadItem(data.itemIdentifier);
+                }
+            })
+            .on('destroy', function() {
+                QUnit.start();
+            });
+    });
+
+
+    QUnit.asyncTest('integration', function (assert) {
+        var serviceCallId = 'previewer';
+        var itemRef = 'item-1';
+        var config = {
+            serviceCallId: serviceCallId,
+            plugins: [{
+                module: 'taoQtiTest/previewer/plugins/navigation/submit',
+                bundle: 'taoQtiTest/loader/qtiPreviewer.min',
+                category: 'navigation'
+            }, {
+                module: 'taoQtiTest/runner/plugins/tools/itemThemeSwitcher/itemThemeSwitcher',
+                bundle: 'taoQtiTest/loader/testPlugins.min',
+                category: 'tools'
+            }]
+        };
+
+        QUnit.expect(1);
+
+        $.mockjax({
+            url: '/init*',
+            responseText: {
+                success: true
+            }
+        });
+        $.mockjax({
+            url: '/getItem*',
+            responseText: {
+                success: true,
+                content: {
+                    type: 'qti',
+                    data: itemData
+                },
+                baseUrl: '',
+                state: {}
+            }
+        });
+
+        previewerFactory(config)
+            .on('error', function (err) {
+                console.error(err);
+                assert.ok(false, 'An error has occurred');
+                QUnit.start();
+            })
+            .on('ready', function (runner) {
+                runner
+                    .after('renderitem.runnerComponent', function () {
+                        assert.ok(true, 'The previewer has been rendered');
+                        QUnit.start();
+                    })
+                    .loadItem(itemRef);
+            });
+    });
+});
