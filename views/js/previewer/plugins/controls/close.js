101d2cf0c5a20d7cce0a437149f9626f68e4e742
jsconan <jsconan@gmail.com>
2018-05-09 17:23:33 +0200

    Add plugin to close the previewer

---
+++ views/js/previewer/plugins/controls/close.js
@@ -0,0 +1,120 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * Test Previewer Control Plugin : Close
+ *
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'i18n',
+    'ui/hider',
+    'taoTests/runner/plugin',
+    'taoQtiTest/runner/helpers/messages',
+    'tpl!taoQtiTest/runner/plugins/templates/button'
+], function ($, _, __, hider, pluginFactory, messages, buttonTpl) {
+    'use strict';
+
+    return pluginFactory({
+
+        name: 'close',
+
+        /**
+         * Initialize the plugin (called during runner's init)
+         */
+        init: function init() {
+            var self = this;
+            var testRunner = this.getTestRunner();
+
+            this.$element = $(buttonTpl({
+                control: 'close',
+                title: __('Close the previewer'),
+                icon: 'close',
+                text: __('Close'),
+                className: 'context-action'
+            }));
+
+            this.$element.on('click', function (e) {
+                e.preventDefault();
+                if (self.getState('enabled') !== false) {
+                    self.disable();
+                    testRunner.trigger('finish');
+                }
+            });
+
+            this.disable();
+
+            testRunner
+                .on('enablenav', function () {
+                    self.enable();
+                })
+                .on('disablenav', function () {
+                    self.disable();
+                });
+        },
+
+        /**
+         * Called during the runner's render phase
+         */
+        render: function render() {
+
+            //attach the element to the navigation area
+            var $container = this.getAreaBroker().getArea('context');
+            $container.append(this.$element);
+        },
+
+        /**
+         * Called during the runner's destroy phase
+         */
+        destroy: function destroy() {
+            this.$element.remove();
+        },
+
+        /**
+         * Enable the button
+         */
+        enable: function enable() {
+            this.$element.removeProp('disabled')
+                .removeClass('disabled');
+        },
+
+        /**
+         * Disable the button
+         */
+        disable: function disable() {
+            this.$element.prop('disabled', true)
+                .addClass('disabled');
+        },
+
+        /**
+         * Show the button
+         */
+        show: function show() {
+            hider.show(this.$element);
+        },
+
+        /**
+         * Hide the button
+         */
+        hide: function hide() {
+            hider.hide(this.$element);
+        }
+    });
+});
---
+++ views/js/previewer/plugins/itemPlugins.js
@@ -0,0 +1,53 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'lodash',
+    'ui/themes',
+    'taoQtiTest/previewer/plugins/controls/close',
+    'taoQtiTest/previewer/plugins/navigation/submit',
+    'taoQtiTest/runner/plugins/tools/itemThemeSwitcher/itemThemeSwitcher'
+], function (_, themeHandler, close, submit, itemThemeSwitcher) {
+    'use strict';
+
+    /**
+     * Gets the list of plugins required to properly preview an item, gathered by categories.
+     * @returns {Object}
+     */
+    return function itemPluginsLoader() {
+        var themesConfig = themeHandler.get('items') || {};
+        var plugins = {
+            controls: [
+                close
+            ],
+            navigation: [
+                submit
+            ]
+        };
+
+        if (themesConfig && _.size(themesConfig.available) > 1) {
+            plugins.tools = [
+                itemThemeSwitcher
+            ];
+        }
+
+        return plugins;
+    };
+});
