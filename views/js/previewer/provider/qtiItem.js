15ad46b8a2ea11abc5cea9a375ca4605dc560cbb
jsconan <jsconan@gmail.com>
2018-05-25 15:04:29 +0200

    Add previewer provider for items

---
+++ views/js/previewer/provider/qtiItem.js
@@ -0,0 +1,66 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'lodash',
+    'taoQtiTest/previewer/item',
+    'css!taoQtiTestCss/item-previewer'
+], function (_, previewerFactory) {
+    'use strict';
+
+    /**
+     * Wraps the legacy item previewer in order to be loaded by the previewer factory
+     */
+    return {
+        name: 'qtiItem',
+
+        /**
+         * Builds and shows the legacy item previewer
+         *
+         * @param {String} uri - The URI of the item to load
+         * @param {Object} state - The state of the item
+         * @param {Object} [config] - Some config entries
+         * @param {String} [config.serviceCallId='previewer'] - The service call Id to send to the server
+         * @param {logger} [config.logger] - A logger for the errors reporting
+         * @param {String} [config.readOnly] - Do not allow to modify the previewed item.
+         * @returns {Object}
+         */
+        init: function init(uri, state, config) {
+            config = _.defaults(config || {}, {
+                serviceCallId: 'previewer',
+                logger: console
+            });
+
+            return previewerFactory(config)
+                .on('error', function (err) {
+                    config.logger.error(err);
+                })
+                .on('ready', function (runner) {
+                    runner
+                        .on('renderitem', function() {
+                            if (state) {
+                                runner.itemRunner.setState(state);
+                            }
+                        })
+                        .loadItem(uri);
+                });
+        }
+    };
+});
