d4943cbd8f396260cec20eef535a4d282839c871
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-05 18:31:48 +0200

    update bundles

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -51,7 +51,8 @@ module.exports = function(grunt) {
                         include : [
                             //everything in runner/plugins
                             'taoQtiTest/runner/plugins/**/*'
-                        ]
+                        ],
+                        dependencies : []
                     }]
                 }
             }
