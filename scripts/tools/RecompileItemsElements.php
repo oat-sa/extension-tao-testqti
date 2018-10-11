f9ce9644a33c4f4cd645cc62fc58953a304ccdc0
Aleksej Tikhanovich <aleksej@taotesting.com>
2018-10-11 11:32:59 +0300

    Code style fixes

--- scripts/tools/RecompileItemsElements.php
+++ scripts/tools/RecompileItemsElements.php
@@ -40,64 +40,92 @@ class RecompileItemsElements extends AbstractAction
 {
     use OntologyAwareTrait;
 
+    /** @var Report */
+    private $report;
+
+    /**
+     * @param $params
+     * @return Report
+     * @throws \common_Exception
+     * @throws \common_exception_Error
+     */
     public function __invoke($params)
     {
+        $this->report = new Report(Report::TYPE_INFO, "Script gracefully ended.");
+
+        $this->recompileItems();
+        
+        return $this->report;
+    }
+
+    /**
+     * @throws \common_Exception
+     * @throws \common_exception_Error
+     */
+    private function recompileItems()
+    {
         $extManager = $this->getServiceLocator()->get(\common_ext_ExtensionsManager::SERVICE_ID);
-        $report = new Report(Report::TYPE_INFO, "Script gracefully ended.");
-            
         if ($extManager->isInstalled('taoDeliveryRdf') === true && $extManager->isEnabled('taoDeliveryRdf') === true) {
-            
+
             $extManager->getExtensionById('taoDeliveryRdf');
-            
+
             $compiledDeliveryClass = $this->getClass(DeliveryAssemblyService::CLASS_URI);
-            
+
             if ($compiledDeliveryClass->exists() === true) {
                 foreach ($compiledDeliveryClass->getInstances(true) as $compiledDelivery) {
-                    /** @var UnionAssignmentService $nccer */
-                    $nccer = $this->getServiceLocator()->get(UnionAssignmentService::SERVICE_ID);
-                    $runtime = $nccer->getRuntime($compiledDelivery);
-
-                    $inputParameters = \tao_models_classes_service_ServiceCallHelper::getInputValues($runtime, []);
-                    $testDefinition = \taoQtiTest_helpers_Utils::getTestDefinition($inputParameters['QtiTestCompilation']);
-
-                    $assessmentItemRefs = $testDefinition->getComponentsByClassName('assessmentItemRef');
-
-                    $report->add(new Report(Report::TYPE_INFO, "Starting to recompile items for delivery {$compiledDelivery->getLabel()} with identifier {$compiledDelivery->getUri()}:"));
-                    $count = 0;
-                    /** @var AssessmentItemRef $assessmentItemRef */
-                    foreach ($assessmentItemRefs as $assessmentItemRef) {
-                        $href = $assessmentItemRef->getHref();
-                        $directoryIds = explode('|', $href);
-                        $itemId = $directoryIds[0];
-                        $item = $this->getResource($itemId);
-                        $uri = $item->getUri();
-                        $identifier = $assessmentItemRef->getIdentifier();
-                        $languages = $item->getUsedLanguages($this->getProperty(\taoItems_models_classes_ItemsService::PROPERTY_ITEM_CONTENT));
-                        $triples = $item->getRdfTriples();
-                        $properties = [];
-                        foreach ($triples as $triple){
-                            $properties[$triple->predicate] = $triple->object;
-                        }
-                        if ($properties) {
-                            $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryIds[2]);
-                        }
-                        foreach ($languages as $lang) {
-                            if (!$directory->has($lang.DIRECTORY_SEPARATOR.QtiJsonItemCompiler::METADATA_FILE_NAME)) {
-                                $directory->write($lang.DIRECTORY_SEPARATOR.QtiJsonItemCompiler::METADATA_FILE_NAME, json_encode($properties));
-                                $report->add(new Report(Report::TYPE_SUCCESS, "Metadata for assessmentItemRef '{$identifier}' with identifier '{$uri}' and lang '{$lang}' successfully compiled."));
-                                $count++;
-                            }
-                        }
-                    }
-                    $report->add(new Report(Report::TYPE_INFO, "Was updated {$count} items."));
+                    $this->getAssessmentsFromDelivery($compiledDelivery);
                 }
             }
         } else {
-            $report->add(
+            $this->add(
                 new Report(Report::TYPE_WARNING, "Extension taoDeliveryRdf is not installed. No compilation environment is available.")
             );
         }
-        
-        return $report;
+    }
+
+    /**
+     * @param $compiledDelivery
+     * @throws \common_Exception
+     * @throws \common_exception_Error
+     */
+    private function getAssessmentsFromDelivery($compiledDelivery)
+    {
+        /** @var UnionAssignmentService $unionAssignmentService */
+        $unionAssignmentService = $this->getServiceLocator()->get(UnionAssignmentService::SERVICE_ID);
+        $runtime = $unionAssignmentService->getRuntime($compiledDelivery);
+
+        $inputParameters = \tao_models_classes_service_ServiceCallHelper::getInputValues($runtime, []);
+        $testDefinition = \taoQtiTest_helpers_Utils::getTestDefinition($inputParameters['QtiTestCompilation']);
+
+        $assessmentItemRefs = $testDefinition->getComponentsByClassName('assessmentItemRef');
+
+        $this->report->add(new Report(Report::TYPE_INFO, "Starting to recompile items for delivery {$compiledDelivery->getLabel()} with identifier {$compiledDelivery->getUri()}:"));
+        $count = 0;
+        /** @var AssessmentItemRef $assessmentItemRef */
+        foreach ($assessmentItemRefs as $assessmentItemRef) {
+            $href = $assessmentItemRef->getHref();
+            $directoryIds = explode('|', $href);
+            $itemId = $directoryIds[0];
+            $item = $this->getResource($itemId);
+            $uri = $item->getUri();
+            $identifier = $assessmentItemRef->getIdentifier();
+            $languages = $item->getUsedLanguages($this->getProperty(\taoItems_models_classes_ItemsService::PROPERTY_ITEM_CONTENT));
+            $triples = $item->getRdfTriples();
+            $properties = [];
+            foreach ($triples as $triple){
+                $properties[$triple->predicate] = $triple->object;
+            }
+            if ($properties) {
+                $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryIds[2]);
+            }
+            foreach ($languages as $lang) {
+                if (!$directory->has($lang.DIRECTORY_SEPARATOR.QtiJsonItemCompiler::METADATA_FILE_NAME)) {
+                    $directory->write($lang.DIRECTORY_SEPARATOR.QtiJsonItemCompiler::METADATA_FILE_NAME, json_encode($properties));
+                    $this->report->add(new Report(Report::TYPE_SUCCESS, "Metadata for assessmentItemRef '{$identifier}' with identifier '{$uri}' and lang '{$lang}' successfully compiled."));
+                    $count++;
+                }
+            }
+        }
+        $this->report->add(new Report(Report::TYPE_INFO, "Was updated {$count} items."));
     }
 }
