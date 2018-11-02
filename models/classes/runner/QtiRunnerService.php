50a7dc0d932fbac15cd51e52c6ffbcc61751f22b
alexey <aliakseiv@taotesting.com>
2018-11-02 12:39:16 +0300

    Deleted unnecessary instanceof

--- models/classes/runner/QtiRunnerService.php
+++ models/classes/runner/QtiRunnerService.php
@@ -1204,13 +1204,11 @@ class QtiRunnerService extends ConfigurableService implements RunnerService
                 $result = false;
             }
 
-            if ($context instanceof QtiRunnerServiceContext) {
-                /** @var ToolsStateStorage $toolsStateStorage */
-                $toolsStateStorage = $this->getServiceManager()->get(ToolsStateStorage::SERVICE_ID);
-                $toolsStates = $toolsStateStorage->deleteStates($context->getTestExecutionUri());
+            /** @var ToolsStateStorage $toolsStateStorage */
+            $toolsStateStorage = $this->getServiceManager()->get(ToolsStateStorage::SERVICE_ID);
+            $toolsStates = $toolsStateStorage->deleteStates($context->getTestExecutionUri());
 
-                $response['tools_states'] = $toolsStates;
-            }
+            $response['tools_states'] = $toolsStates;
 
             $this->getServiceManager()->get(ExtendedStateService::SERVICE_ID)->clearEvents($executionUri);
         } else {
