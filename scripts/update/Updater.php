9cbd9cdef9ef6a56d609de181b0bc7017fa8d5c1
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-11-29 22:03:39 +0100

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1682,6 +1682,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('26.1.2');
         }
 
-        $this->skip('26.1.2', '29.2.0');
+        $this->skip('26.1.2', '30.0.0');
     }
 }
