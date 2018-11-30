ca4e9c694a7c574c5e48d0f3cb268bc3d68a9961
uncleempty <ilya.yarkavets@1pt.com>
2018-11-30 13:37:13 +0300

    Update config in updater

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -74,6 +74,7 @@ use oat\libCat\custom\EchoAdaptEngine;
 use oat\taoTests\models\runner\providers\ProviderRegistry;
 use oat\taoTests\models\runner\providers\TestProvider;
 use oat\taoQtiTest\models\compilation\CompilationService;
+use oat\taoTests\models\runner\time\TimePoint;
 
 /**
  *
@@ -1682,6 +1683,22 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('26.1.2');
         }
 
-        $this->skip('26.1.2', '29.4.1');
+        $this->skip('26.1.2', '29.4.0');
+
+        if ($this->isVersion('29.4.1')) {
+            $extension = $this->getServiceManager()->get(\common_ext_ExtensionsManager::SERVICE_ID)->getExtensionById('taoQtiTest');
+            $config = $extension->getConfig('testRunner');
+
+            $currentTarget = $config['timer']['target'];
+
+            if ($currentTarget === 'client') {
+                $config['timer']['target'] = TimePoint::TARGET_CLIENT;
+            } else {
+                $config['timer']['target'] = TimePoint::TARGET_SERVER;
+            }
+            
+            $extension->setConfig('testRunner', $config);
+            $this->setVersion('29.4.1');
+        }
     }
 }
