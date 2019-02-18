123364e51d534eaffe3ebca07b54afed79f4e73a
ricproenca-tao <ricardo@taotesting.com>
2019-02-18 16:54:48 +0100

    version bump to 30.5.3

--- manifest.php
+++ manifest.php
@@ -39,7 +39,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '30.5.2',
+    'version'     => '30.5.3',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoQtiItem' => '>=18.0.0',
