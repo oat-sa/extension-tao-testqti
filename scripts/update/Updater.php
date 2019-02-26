a8d3a3ea1ef11da4fbb4c6af7ecfb93fca9a7b40
uncleempty <ilya.yarkavets@1pt.com>
2019-02-26 21:18:55 +0300

    Correct versioning (in one of the previous commits dependency major version was bumped)

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1759,6 +1759,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('30.7.0');
         }
 
-        $this->skip('30.7.0', '30.7.2');
+        $this->skip('30.7.0', '31.0.0');
     }
 }
