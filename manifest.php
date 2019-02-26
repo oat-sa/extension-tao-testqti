0de61a7a5d82141e0eb5837634b8a2f46db89851
rKârolis <karolis@taotesting.com>
2019-02-26 19:03:35 +0100

    version bump

--- manifest.php
+++ manifest.php
@@ -39,7 +39,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '30.7.0',
+    'version'     => '30.7.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoQtiItem' => '>=18.0.0',
