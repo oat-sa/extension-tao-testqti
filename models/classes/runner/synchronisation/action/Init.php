3294d9407ee7de65345b4d6bce55c45994a45466
Mikhail Kamarouski <kamarouski@1pt.com>
2019-03-26 11:16:37 +0300

    Initial commit; communication action placeholders added;

---
+++ models/classes/runner/synchronisation/action/Init.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA ;
+ */
+
+namespace oat\taoQtiTest\models\runner\synchronisation\action;
+
+use oat\taoQtiTest\models\runner\synchronisation\TestRunnerAction;
+
+/**
+ * @package oat\taoQtiTest\models\runner\synchronisation\action
+ */
+class Init extends TestRunnerAction
+{
+    /**
+     * Get item data by identifier
+     *
+     * Validate required fields.
+     * Get item data by given identifier
+     *
+     * @return array
+     * @throws
+     */
+    public function process()
+    {
+
+    }
+
+}
