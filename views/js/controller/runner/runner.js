adc1f2c6e6e29fd768da1a40709f1441c333b441
Aleh Hutnikau <hutnikau@1pt.com>
2019-04-29 11:24:22 +0300

    do not log test runner options

--- views/js/controller/runner/runner.js
+++ views/js/controller/runner/runner.js
@@ -115,7 +115,7 @@ define([
             var runnerOptions = _.defaults({}, options, defaults);
             var exitReason;
             var $container = $('.runner');
-            var logger     = loggerFactory('controller/runner', { runnerOptions : runnerOptions });
+            var logger     = loggerFactory('controller/runner');
             var preventFeedback = false;
             var errorFeedback = null;
 
