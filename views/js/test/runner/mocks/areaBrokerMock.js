b17672d4b90bccefdd15d4ef1e251c4ead765bae
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-06-11 16:45:21 +0200

    replace the areaBroker mock dependency by the actual mock

--- views/js/test/runner/mocks/areaBrokerMock.js
+++ views/js/test/runner/mocks/areaBrokerMock.js
@@ -17,37 +17,35 @@
  */
 /**
  * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
+ * @author Christophe Noël <christophe@taotesting.com>
  */
 define([
     'jquery',
     'lodash',
-    'tao/test/ui/areaBroker/mock/areaBrokerMock',
+    'ui/areaBroker',
     'taoQtiTest/runner/ui/toolbox/toolbox'
-], function ($, _, areaBrokerMockFactory, toolboxFactory) {
+], function($, _, areaBrokerFactory, toolboxFactory) {
     'use strict';
 
     /**
-     * The mock
-     * @type {Object}
+     * A counter utilised to generate the mock identifiers
+     * @type {Number}
      */
-    var areaBroker;
+    let mockId = 0;
 
-    /**
-     * The list of default areas
-     * @type {String[]}
-     */
-    var defaultAreas = [
-        'content',      //where the content is renderer, for example an item
-        'toolbox',      //the place to add arbitrary tools, like a zoom, a comment box, etc.
-        'navigation',   //the navigation controls like next, previous, skip
-        'control',      //the control center of the test, progress, timers, etc.
-        'header',       //the area that could contains the test titles
-        'panel'         //a panel to add more advanced GUI (item review, navigation pane, etc.)
-    ];
+    const classes = {
+        areaBroker: 'area-broker-mock',
+        area: 'test-area'
+    };
+
+    const defaultContainerSelector = '#qunit-fixture';
 
     /**
      * Builds and returns a new areaBroker with dedicated areas.
-     * @param config.$brokerContainer - where to create the area broker - default to #qunit-fixture
+     * @param {Object} [config]
+     * @param {String} config.id - area broker id, will be used as a class on container
+     * @param {String[]} config.defaultAreas - mandatory areas to create
+     * @param {jQuery} config.$brokerContainer - where to create the area broker - default to #qunit-fixture
      * @param {String[]} config.areas - A list of areas to create, or...
      * @param {Object} config.mapping - ... a list of already created areas
      * @returns {areaBroker} - Returns the new areaBroker
@@ -55,15 +53,61 @@ define([
     function areaBrokerMock(config) {
 
         config = _.defaults(config || {}, {
-            defaultAreas: defaultAreas,
+            defaultAreas: [
+                'content', //where the content is renderer, for example an item
+                'toolbox', //the place to add arbitrary tools, like a zoom, a comment box, etc.
+                'navigation', //the navigation controls like next, previous, skip
+                'control', //the control center of the test, progress, timers, etc.
+                'header', //the area that could contains the test titles
+                'panel' //a panel to add more advanced GUI (item review, navigation pane, etc.)
+            ],
             id: 'test-runner'
         });
 
-        areaBroker = areaBrokerMockFactory(config);
+        const $areaBrokerDom = $('<div />')
+            .attr('id', `area-broker-mock-${mockId++}`)
+            .addClass(config.id || classes.areaBroker);
 
+
+        // Create all areas from scratch
+        if (!config.mapping) {
+            config.mapping = {};
+            if (!config.areas) {
+                config.areas = config.defaultAreas;
+            } else {
+                config.areas = _.keys(_.merge(_.object(config.areas), _.object(config.defaultAreas)));
+            }
+
+            _.forEach(config.areas, areaId => {
+                config.mapping[areaId] = $('<div />')
+                    .addClass('test-area')
+                    .addClass(areaId)
+                    .appendTo($areaBrokerDom);
+            });
+
+            // Create only missing areas
+        } else {
+            _.union(config.defaultAreas, (config.areas || [])).forEach( areaId => {
+                // create missing areas
+                if (!config.mapping[areaId]) {
+                    config.mapping[areaId] = $('<div />')
+                        .addClass('test-area')
+                        .addClass(areaId)
+                        .appendTo($areaBrokerDom);
+                }
+            });
+        }
+
+        if (!config.$brokerContainer) {
+            config.$brokerContainer = $(defaultContainerSelector);
+        }
+        config.$brokerContainer.append($areaBrokerDom);
+
+        const areaBroker = areaBrokerFactory(config.defaultAreas, $areaBrokerDom, config.mapping);
         areaBroker.setComponent('toolbox', toolboxFactory().init());
 
         return areaBroker;
+
     }
 
     return areaBrokerMock;
