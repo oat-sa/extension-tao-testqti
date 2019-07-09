5d377e391839dad6ea4283b7a3ec38b516d1e04b
Tamas Besenyei <tamas@besenyei.net>
2019-07-09 13:51:47 +0200

    fix scss paths

--- views/build/grunt/sass.js
+++ views/build/grunt/sass.js
@@ -7,13 +7,13 @@ module.exports = function(grunt) {
     grunt.config.merge({
         sass : {
             options: {
-                includePaths : [ root + '../../node_modules/@oat-sa/tao-test-runner-qti/scss' ]
+                includePaths : [ root + 'node_modules/@oat-sa/tao-test-runner-qti/scss' ]
             },
             taoqtitest: {
                 files : [
                     { dest : root + 'css/creator.css', src : root + 'scss/creator.scss' },
                     { dest : root + 'css/test-runner.css', src : root + 'scss/test-runner.scss' },
-                    { dest : root + 'css/new-test-runner.css', src :  root + 'scss/new-test-runner.scss'},
+                    { dest : root + 'node_modules/@oat-sa/tao-test-runner-qti/scss/new-test-runner.css', src :  root + 'scss/new-test-runner.scss'},
                     { dest : pluginDir + 'controls/timer/component/css/countdown.css', src : pluginDir + 'controls/timer/component/scss/countdown.scss'},
                     { dest : pluginDir + 'controls/timer/component/css/timerbox.css', src : pluginDir + 'controls/timer/component/scss/timerbox.scss'}
                 ]
