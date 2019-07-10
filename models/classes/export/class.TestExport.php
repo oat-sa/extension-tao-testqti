7d4abe55502bd3630bc71b6cbca524de95a6e728
Razhkou <yuryr@taotesting.com>
2019-07-10 18:07:37 +0300

    TAO-6008 v2

--- models/classes/export/class.TestExport.php
+++ models/classes/export/class.TestExport.php
@@ -115,11 +115,9 @@ class taoQtiTest_models_classes_export_TestExport implements tao_models_classes_
 
         if (!$report->containsError() && $subjectUri) {
             $this->getEventManager()->trigger(new QtiTestExportEvent(new core_kernel_classes_Resource($subjectUri)));
-
-            $report->setData($path);
             $report->setMessage(__('Resource(s) successfully exported.'));
         }
-
+        $report->setData($path);
         return $report;
     }
 
