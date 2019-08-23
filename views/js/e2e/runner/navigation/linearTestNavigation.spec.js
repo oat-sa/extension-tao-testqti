a28cf25e5102cbdabfc906529a7b6b9bfd5124e7
Ricardo <ricardo@taotesting.com>
2019-08-23 18:20:52 +0200

    add test for navigation non linear test

--- views/js/e2e/runner/navigation/linearTestNavigation.spec.js
+++ views/js/e2e/runner/navigation/linearTestNavigation.spec.js
@@ -186,4 +186,161 @@ describe('Deliveries', () => {
 
     });
 
+    describe('Non Linear Test Navigation', () => {
+
+        /**
+         * Setup to have a proper delivery:
+         * - Start server
+         * - Add necessary routes
+         * - Admin login
+         * - Import and publish e2e example test
+         * - Set guest access on delivery and save
+         * - Logout
+         */
+        before(() => {
+            cy.setupServer();
+            cy.addRoutes();
+            cy.addTestRoutes();
+            cy.login('admin');
+            cy.importTestPackage(base64Test.nonLinearTest);
+            cy.publishTest('e2e navigation non linear test');
+            cy.setDeliveryForGuests('Delivery of e2e navigation non linear test');
+            cy.logout();
+        });
+
+        /**
+         * Setup to have a proper delivery:
+         * - Start server
+         * - Add necessary routes
+         * - Guest login
+         * - Start test
+         */
+        beforeEach(() => {
+            cy.setupServer();
+            cy.addRoutes();
+            cy.addTestRoutes();
+            cy.guestLogin();
+            cy.startTest('Delivery of e2e navigation non linear test');
+        });
+
+        /**
+         * Destroy everything we created during setup, leaving the environment clean for next time.
+         */
+        after(() => {
+            cy.setupServer();
+            cy.addRoutes();
+            cy.login('admin');
+            cy.deleteItem('e2e navigation non linear test');
+            cy.deleteTest('e2e navigation non linear test');
+            cy.deleteDelivery('Delivery of e2e navigation non linear test');
+        });
+
+        describe('First Item', () => {
+
+            it('Has proper navigation buttons', function () {
+                cy.get('.navi-box-list').within(() => {
+                    // visible navigation buttons
+                    cy.get(setupSelectors.testNavigation.nextItem).should('exist').and('be.visible');
+                    cy.get(setupSelectors.testNavigation.skipItem).should('exist').and('be.visible');
+
+                    // not visible navigation buttons
+                    cy.get(setupSelectors.testNavigation.previousItem).should('exist').and('not.be.visible');
+
+                    //not existing buttons
+                    cy.get(setupSelectors.testNavigation.endTest).should('not.exist');
+                    cy.get(setupSelectors.testNavigation.skipAndEndTest).should('not.exist');
+                });
+            });
+
+            it('Move to next item', cy.nextItem);
+
+            it('Skip to next item', cy.skipItem);
+
+        });
+
+        describe('Second Item', () => {
+
+            it('Has proper navigation buttons', function () {
+                // go to 2nd item
+                cy.nextItem();
+
+                cy.get('.navi-box-list').within(() => {
+                    // visible navigation buttons
+                    cy.get(setupSelectors.testNavigation.nextItem).should('exist').and('be.visible');
+                    cy.get(setupSelectors.testNavigation.skipItem).should('exist').and('be.visible');
+                    cy.get(setupSelectors.testNavigation.previousItem).should('exist').and('be.visible');
+
+                    //not existing buttons
+                    cy.get(setupSelectors.testNavigation.endTest).should('not.exist');
+                    cy.get(setupSelectors.testNavigation.skipAndEndTest).should('not.exist');
+                });
+            });
+
+            it('Move to next item', () => {
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+            });
+
+            it('Skip to next item', () => {
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+            });
+        });
+
+        describe('Last Item', () => {
+            it('Has proper navigation buttons', function () {
+
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+
+                cy.get('.navi-box-list').within(() => {
+                    // visible navigation buttons
+                    cy.get(setupSelectors.testNavigation.endTest).should('exist').and('be.visible');
+                    cy.get(setupSelectors.testNavigation.skipAndEndTest).should('exist').and('be.visible');
+                    cy.get(setupSelectors.testNavigation.previousItem).should('exist').and('be.visible');
+
+                    //not existing buttons
+                    cy.get(setupSelectors.testNavigation.skipItem).should('not.exist');
+                    cy.get(setupSelectors.testNavigation.nextItem).should('not.exist');
+                });
+            });
+
+            it('Move to previous item', () => {
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+                cy.previousItem();
+            });
+
+            it('End test', () => {
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+                cy.endTest();
+
+                //check if the test is really ended
+                cy.location().should((loc) => {
+                    expect(loc.pathname).to.eq(runnerUrls.availableDeliveriesPageUrl);
+                });
+            });
+
+            it('Skip and end test', () => {
+                // go to last item
+                cy.nextItem();
+                cy.nextItem();
+                cy.skipAndEndTest();
+
+                //check if the test is really ended
+                cy.location().should((loc) => {
+                    expect(loc.pathname).to.eq(runnerUrls.availableDeliveriesPageUrl);
+                });
+            });
+
+        });
+
+    });
+
 });
