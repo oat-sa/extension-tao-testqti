69dd6c3ac1743b7f07420b633d3ed4df05e710e2
Ricardo <ricardo@taotesting.com>
2019-09-12 17:14:13 +0200

    add new tests for extended text interaction

--- views/js/e2e/interactions/extendedtext/extendedtext.spec.js
+++ views/js/e2e/interactions/extendedtext/extendedtext.spec.js
@@ -16,11 +16,15 @@
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA ;
  */
 
-import {commonInteractionSelectors, extendedTextInteractionSelectors} from '../../_helpers/selectors/interactionSelectors';
+import {
+    commonInteractionSelectors,
+    extendedTextInteractionSelectors
+} from '../../_helpers/selectors/interactionSelectors';
 
 import '../../_helpers/commands/setupCommands';
 import '../../_helpers/commands/cleanupCommands';
 import '../../_helpers/commands/pointerCommands';
+import '../../_helpers/commands/navigationCommands';
 
 import '../../_helpers/routes/backOfficeRoutes';
 import '../../_helpers/routes/runnerRoutes';
@@ -77,22 +81,58 @@ describe('Interactions', () => {
     describe('Extended text interaction', () => {
 
         it('Loads in proper state', function () {
-            cy.get(commonInteractionSelectors.interaction).within(() => {
+            cy.get(commonInteractionSelectors.interaction).eq(0).within(() => {
                 cy.get(extendedTextInteractionSelectors.textContainer).should('exist').and('be.visible');
-                cy.get(extendedTextInteractionSelectors.countChars).should('exist').and('be.visible').and('have.text', '0');
+                cy.get(extendedTextInteractionSelectors.countChars).should('exist').and('be.visible').and('have.text', '00');
             });
         });
 
         it('Counter increase when text is added', function () {
-            cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World!');
-            cy.get(extendedTextInteractionSelectors.countChars).should('have.text', 'Hello, World!'.length.toString());
+            cy.get(commonInteractionSelectors.interaction).eq(0).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World!');
+
+                cy.get(extendedTextInteractionSelectors.countChars).eq(0).should('have.text', 'Hello, Wor'.length.toString());
+                cy.get(extendedTextInteractionSelectors.countChars).eq(1).should('have.text', 'Hello, Wor'.length.toString());
+            });
         });
 
         it('Counter decrease when text is deleted', function () {
-            cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World!');
-            cy.get(extendedTextInteractionSelectors.textContainer).type('{backspace}').type('{backspace}').type('{backspace}');
-            cy.get(extendedTextInteractionSelectors.countChars).should('have.text', 'Hello, World!'.slice(0, -3).length.toString());
+            cy.get(commonInteractionSelectors.interaction).eq(0).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World!');
+                cy.get(extendedTextInteractionSelectors.textContainer).type('{backspace}').type('{backspace}').type('{backspace}');
+
+                cy.get(extendedTextInteractionSelectors.countChars).eq(0).should('have.text', 'Hello, Wor'.slice(0, -3).length.toString());
+                cy.get(extendedTextInteractionSelectors.countChars).eq(1).should('have.text', 'Hello, Wor'.slice(0, -3).length.toString());
+            });
         });
 
+        it('Character limit reached', function () {
+            cy.get(commonInteractionSelectors.interaction).eq(0).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World! Hello, World! Hello, World! Hello, World!');
+                cy.get(extendedTextInteractionSelectors.textContainer).should('have.value', 'Hello, Wor');
+            });
+        });
+
+        it('Word limit reached', function () {
+            cy.get(commonInteractionSelectors.interaction).eq(1).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).type('Hello, World! Hello, World! Hello, World! Hello, World!');
+                cy.get(extendedTextInteractionSelectors.textContainer).should('have.value', 'Hello, World!Hello,World!Hello,World!Hello,World!');
+            });
+        });
+
+        it('Interaction keeps state', function () {
+            cy.get(commonInteractionSelectors.interaction).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).eq(0).type('Hello Wor');
+                cy.get(extendedTextInteractionSelectors.textContainer).eq(1).type('World Hello');
+            });
+
+            cy.nextItem();
+            cy.previousItem();
+
+            cy.get(commonInteractionSelectors.interaction).within(() => {
+                cy.get(extendedTextInteractionSelectors.textContainer).eq(0).should('have.value', 'Hello Wor');
+                cy.get(extendedTextInteractionSelectors.textContainer).eq(1).should('have.value', 'World Hello');
+            });
+        });
     });
 });
