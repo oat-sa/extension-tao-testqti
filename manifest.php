a19953f55b254a5ca5217338bd3a1a8cf159f6b3
Pavel Handleman <pavel.handleman@gmail.com>
2019-09-24 14:05:57 +0300

    TAO-9218 composer

--- manifest.php
+++ manifest.php
@@ -41,7 +41,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '34.14.0',
+    'version'     => '34.14.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoQtiItem' => '>=20.0.2',
