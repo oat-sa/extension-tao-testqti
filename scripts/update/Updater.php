7ac48eca30728669a2ec7867c90d02d4e2294977
Anton Tsymuk <atsymuk@gmail.com>
2019-12-17 14:25:10 +0300

    update version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1970,6 +1970,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('35.2.0');
         }
 
-        $this->skip('35.2.0', '35.2.4');
+        $this->skip('35.2.0', '35.3.0');
     }
 }
