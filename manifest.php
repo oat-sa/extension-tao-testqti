62f3dbd952c94c0369e751b00a0dd17d7bcd3ac0
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-02-12 18:49:20 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -42,7 +42,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '35.10.0',
+    'version'     => '35.11.0',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=20.0.2',
