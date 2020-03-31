7ba67df1062f167d42d4072bebce045c7bce7377
Julien Sébire <julien@taotesting.com>
2020-03-31 16:40:37 +0200

    Version bump.

--- manifest.php
+++ manifest.php
@@ -47,7 +47,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '36.7.0',
+    'version'     => '36.7.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=23.9.2',
