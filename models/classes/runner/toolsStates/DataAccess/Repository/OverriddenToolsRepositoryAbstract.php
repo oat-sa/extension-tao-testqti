bb927dc1c4d6a23a006a52d96dd2758b3beffef9
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-04-20 12:09:42 +0200

    BSA-81 chore(annotation): add `@author` annotations to the new files

--- models/classes/runner/toolsStates/DataAccess/Repository/OverriddenToolsRepositoryAbstract.php
+++ models/classes/runner/toolsStates/DataAccess/Repository/OverriddenToolsRepositoryAbstract.php
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ *
+ * @author Sergei Mikhailov <sergei.mikhailov@taotesting.com>
  */
 declare(strict_types=1);
 
