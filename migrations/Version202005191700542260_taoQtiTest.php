c6c4d7103742d7f1f69455964f75ae3675cccd04
bugalot <bugalot1985@gmail.com>
2020-05-19 19:02:09 +0200

    Added second migration.

---
+++ migrations/Version202005191700542260_taoQtiTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoQtiTest\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\oatbox\log\LoggerService;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\oatbox\service\ServiceManagerAwareInterface;
+use oat\oatbox\service\ServiceManagerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202005191700542260_taoQtiTest extends AbstractMigration implements ServiceManagerAwareInterface
+{
+    use ServiceManagerAwareTrait;
+
+    public function getDescription(): string
+    {
+        return 'A test migration for extension taoQtiTest (2).';
+    }
+
+    public function up(Schema $schema): void
+    {
+        /** @var LoggerService $logger */
+        $logger = $this->getServiceLocator()->get(LoggerService::LOGGER_OPTION);
+        $logger->debug("taoQtiTest Migration 2 UP.");
+    }
+
+    public function down(Schema $schema): void
+    {
+        /** @var LoggerService $logger */
+        $logger = $this->getServiceLocator()->get(LoggerService::LOGGER_OPTION);
+        $logger->debug("taoQtiTest Migration 2 DOWN.");
+    }
+}
