45e640c401b90272d511585f10210f010111d98f
lecosson <lecosson@gmail.com>
2020-05-28 14:08:03 +0300

    fix mathjax elements background color with tts

--- manifest.php
+++ manifest.php
@@ -48,7 +48,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '37.11.2',
+    'version'     => '37.11.3',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
