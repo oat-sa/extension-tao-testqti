eb5cfd34c40f61837fbc984dca8414d4f3f1285a
jsconan <jsconan@gmail.com>
2020-05-28 18:47:12 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -48,7 +48,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '37.11.3',
+    'version'     => '37.11.4',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
