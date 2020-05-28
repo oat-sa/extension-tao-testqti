92c5bc6a26af8ad6ddc85b417c5c6cd95947e5fb
Oleksandr Khitko <oleksandr@taotesting.com>
2020-05-28 16:18:35 +0200

    Lock existing session in retrieve method

--- helpers/class.TestSessionStorage.php
+++ helpers/class.TestSessionStorage.php
@@ -131,13 +131,12 @@ class taoQtiTest_helpers_TestSessionStorage extends AbstractQtiBinaryStorage imp
      */
     public function retrieve(AssessmentTest $test, $sessionId, $forReadingOnly = false)
     {
-        if (static::$session && static::$session->getSessionId() === $sessionId) {
-            return static::$session;
+        if (!$this->sessionExists($sessionId)) {
+            $this->setLastError(-1);
+            static::$session = parent::retrieve($test, $sessionId);
+            static::$session->setReadOnly($forReadingOnly);
         }
 
-        $this->setLastError(-1);
-        static::$session = parent::retrieve($test, $sessionId);
-        static::$session->setReadOnly($forReadingOnly);
         if (!$forReadingOnly) {
             $this->lockSession(static::$session);
         }
@@ -178,6 +177,10 @@ class taoQtiTest_helpers_TestSessionStorage extends AbstractQtiBinaryStorage imp
      */
     public function lockSession(AssessmentTestSession $session)
     {
+        if ($session->isLocked()) {
+            return;
+        }
+
         $lock = $this->createLock('AssessmentTestSession_' . $session->getSessionId(), 30);
         $lock->acquire(true);
         $session->setReadOnly(false);
@@ -266,5 +269,14 @@ class taoQtiTest_helpers_TestSessionStorage extends AbstractQtiBinaryStorage imp
             return ServiceManager::getServiceManager();
         }
         return $this->serviceLocator;
+    }
+
+    /**
+     * @param string $sessionId
+     * @return bool
+     */
+    private function sessionExists(string $sessionId): bool
+    {
+        return static::$session && static::$session->getSessionId() === $sessionId;
     }
 }
