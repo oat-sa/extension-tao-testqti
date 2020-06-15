c14d14662b0a6dd30bfd80f626e14285052f22a9
Aleksej Tikhanovich <aleksej@taotesting.com>
2020-06-15 09:31:33 +0300

    small code style issues

--- actions/class.XmlEditor.php
+++ actions/class.XmlEditor.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,7 +22,7 @@
 
 class taoQtiTest_actions_XmlEditor extends tao_actions_ServiceModule
 {
-   public function edit()
+   public function edit() : void
    {
        $this->setView('XmlEditor/xml_editor.tpl');
    }
