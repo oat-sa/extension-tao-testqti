d2c9df426b6560e2e726524520275633f4db15a7
Aleksej Tikhanovich <aleksej@taotesting.com>
2020-06-16 11:34:27 +0300

    Code style fixes

--- config/default/XmlEditor.conf.php
+++ config/default/XmlEditor.conf.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,6 +18,8 @@ declare(strict_types=1);
  *
  *
  */
+
+declare(strict_types=1);
 
 use oat\taoQtiTest\models\xmlEditor\XmlEditor;
 
