8e57cffe247e2bb52d462ef02b5f72b267af00f2
Aleksej Tikhanovich <aleksej@taotesting.com>
2020-06-16 17:26:55 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -49,7 +49,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '38.8.0',
+    'version'     => '38.9.0',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
