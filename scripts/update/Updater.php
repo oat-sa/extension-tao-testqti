97989bfc7ee2a36928ba80b3a639455cc34e6f66
Anton Tsymuk <atsymuk@gmail.com>
2020-06-18 09:41:59 +0300

    update version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -2115,6 +2115,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('38.7.0');
         }
 
-        $this->skip('38.7.0', '38.10.3');
+        $this->skip('38.7.0', '38.10.4');
     }
 }
