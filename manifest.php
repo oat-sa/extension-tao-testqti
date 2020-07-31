8efe5197afe2f29e86a4ff91d63525775ecb6e4a
Anton Tsymuk <atsymuk@gmail.com>
2020-07-31 10:16:55 +0300

    update version

--- manifest.php
+++ manifest.php
@@ -50,7 +50,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '39.0.2',
+    'version'     => '39.1.0',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
