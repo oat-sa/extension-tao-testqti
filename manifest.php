311fd6b2aef39e9ad7c8eee548b5c4339ccceec9
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-08-05 17:13:38 +0200

    Update manifest.php
    Co-authored-by: Sergei Mikhailov <sergei.mikhailov@taotesting.com>

--- manifest.php
+++ manifest.php
@@ -50,7 +50,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '39.3.1',
+    'version'     => '39.4.0',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
