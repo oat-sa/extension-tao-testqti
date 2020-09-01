67bbef8a87fdd60b6e66b4af146a8f34b9799cc3
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-09-01 12:52:11 +0200

    BSA-231 chore(version): bump a fix one

--- manifest.php
+++ manifest.php
@@ -50,7 +50,7 @@ return [
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '39.7.4',
+    'version'     => '39.7.5',
     'author'      => 'Open Assessment Technologies',
     'requires'    => [
         'taoQtiItem' => '>=24.0.0',
