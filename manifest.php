e23eee0276f176dec276eed699a4cc2bc36e9de3
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-09-28 15:44:34 +0200

    TBD-63 chore(version): bump a hotfix one

--- manifest.php
+++ manifest.php
@@ -41,7 +41,7 @@ return array(
     'label'       => 'QTI test model',
     'description' => 'TAO QTI test implementation',
     'license'     => 'GPL-2.0',
-    'version'     => '35.2.2',
+    'version'     => '35.2.2.1',
     'author'      => 'Open Assessment Technologies',
     'requires'    => array(
         'taoQtiItem' => '>=20.0.2',
