b0a1ebff9846aa60b4341c1dd8c1e75f97ccd81b
Andrei Shapiro <shpr.andrew@gmail.com>
2021-04-21 17:14:10 +0300

    refactor: sanitize in a separate class and add unit tests

---
+++ test/unit/helpers/QtiTestSanitizerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoQtiTest\test\unit\helpers;
+
+use oat\generis\test\TestCase;
+use oat\taoQtiTest\helpers\QtiTestSanitizer;
+
+class QtiTestSanitizerTest extends TestCase
+{
+    /**
+     * @dataProvider sanitizeContentProvider
+     */
+    public function testSanitizeContent(string $content, string $expected): void
+    {
+        $this->assertEquals($expected, QtiTestSanitizer::sanitizeContent($content));
+    }
+
+    public function sanitizeContentProvider(): array
+    {
+        return [
+            [
+                'content' => '<script>alert(123);</script>',
+                'expected' => '',
+            ],
+            [
+                'content' => '<script defer>alert(123);</script>',
+                'expected' => '',
+            ],
+            [
+                'content' => '<script>' . PHP_EOL . 'alert(123);' . PHP_EOL . '</script>',
+                'expected' => '',
+            ],
+            [
+                'content' => '<script>alert(123);<script>alert(123);</script></script>',
+                'expected' => '',
+            ],
+            [
+                'content' => '<div>content</div>',
+                'expected' => '&lt;div&gt;content&lt;/div&gt;',
+            ],
+            [
+                'content' => '<div>content<script>alert(123);</script></div>',
+                'expected' => '&lt;div&gt;content&lt;/div&gt;',
+            ],
+            [
+                'content' => '<div onload="alert(123);">content</div>',
+                'expected' => '&lt;div onload=&quot;alert(123);&quot;&gt;content&lt;/div&gt;',
+            ],
+        ];
+    }
+}
