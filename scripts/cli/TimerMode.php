40f88eb31dcf77a9f23d698681316d30690ce631
jsconan <jsconan@gmail.com>
2022-01-14 11:01:15 +0100

    refactor: improve the TimerMode CLI script
    - refine the internal structure, splitting it into smaller chunks
    - simplify conditions with early returns
    - refine the CLI options by making them clearer
    - add the help command to display the usage
    - document the usage and the purpose

--- scripts/cli/TimerMode.php
+++ scripts/cli/TimerMode.php
@@ -22,27 +22,85 @@ declare(strict_types=1);
 
 namespace oat\taoQtiTest\scripts\cli;
 
+use common_ext_Extension;
+use common_ext_ExtensionsManager;
+use Exception;
+use InvalidArgumentException;
 use oat\oatbox\extension\script\ScriptAction;
 use oat\oatbox\reporting\Report;
 
+/**
+ * Manage the timer-mode option for the TestRunner.
+ * For the time being these modes are supported:
+ * - 'server': The timer is managed by the server. It starts with the test and ends with it.
+ *   Any interruption occurring on the client will be ignored, unless the test is explicitly
+ *   paused by calling the pause action.
+ * - 'client': The timer is managed by the client application. It is synchronized upon each
+ *   navigation move. In case of interruption, the timer is paused and resumed when the item
+ *   is in the interacting state. Time spent outside will be discarded, like while navigating
+ *   between items or waiting for network. Between synchronisation steps the timer value is
+ *   stored in the client storage. If this storage is emptied, the duration elapsed after the
+ *   last synchronisation will be lost. This may happen if the browser's local storage is cleaned,
+ *   or if the test is resumed from a different browser or computer.
+ *
+ * Usage:
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' [options]
+ *
+ *   Optional Arguments:
+ *     -g,       --get        Displays the timer mode
+ *     -s mode, --set mode    Changes the timer mode
+ *     -h,       --help       Prints a help statement
+ *
+ * Examples:
+ * - Displays the current mode:
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' -g
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' --get
+ *
+ * - Change the timer mode to 'client'
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' -s client
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' --set client
+ *
+ * - Displays help:
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' -h
+ *     php index.php '\oat\taoQtiTest\scripts\cli\TimerMode' --help
+ */
 class TimerMode extends ScriptAction
 {
-    public const SET_TIMER_MODE = 'set';
+    public const CONFIG_FILE = 'testRunner';
+    public const GET_TIMER_MODE = 'show';
+    public const SET_TIMER_MODE = 'mode';
     public const SUPPORTED_MODES = [
         'server',
         'client'
     ];
 
+    protected function provideUsage()
+    {
+        return [
+            'prefix' => 'h',
+            'longPrefix' => 'help',
+            'description' => 'Prints a help statement'
+        ];
+    }
+
     protected function provideOptions()
     {
         return [
+            self::GET_TIMER_MODE => [
+                'prefix' => 'g',
+                'longPrefix' => 'get',
+                'cast' => 'string',
+                'flag' => true,
+                'required' => false,
+                'description' => 'Displays the timer mode',
+            ],
             self::SET_TIMER_MODE => [
                 'prefix' => 's',
                 'longPrefix' => 'set',
                 'cast' => 'string',
                 'flag' => false,
                 'required' => false,
-                'description' => 'Set the timer mode',
+                'description' => 'Changes the timer mode',
             ],
         ];
     }
@@ -56,52 +114,62 @@ class TimerMode extends ScriptAction
     {
         $report = Report::createInfo(sprintf('The test runner timer mode is currently set to "%s"', $this->getTimerMode()));
 
-        $mode = $this->getOption(self::SET_TIMER_MODE);
-        if ($mode) {
-            if ($this->setTimerMode($mode)) {
-                $report->add(
-                    Report::createSuccess(
-                        sprintf('The test runner timer mode was successfully set to "%s"', $mode)
-                    )
-                );
-            } else {
-                $report->add(
-                    Report::createError(
-                        sprintf('The test runner timer mode "%s" is not supported and it was not applied', $mode)
-                    )
-                );
-            }
+        if ($this->getOption(self::GET_TIMER_MODE)) {
+            return $report;
+        }
+
+        try {
+            $mode = $this->getOption(self::SET_TIMER_MODE);
+
+            $this->validate($mode);
+
+            $this->setTimerMode($mode);
+
+            $report->add(
+                Report::createSuccess(sprintf('The test runner timer mode was successfully set to "%s"', $mode))
+            );
+        } catch (Exception $exception) {
+            $report->add(
+                Report::createError(sprintf('Failed to change the timer mode: %s', $exception->getMessage()))
+            );
         }
 
         return $report;
     }
 
-    private function getExtensionsManagerService()
+    private function validate(?string $mode): void
     {
-        return $this->getServiceLocator()->get(\common_ext_ExtensionsManager::SERVICE_ID);
+        if (empty($mode)) {
+            throw new InvalidArgumentException('The timer mode must be provided');
+        }
+
+        if (!in_array($mode, self::SUPPORTED_MODES, true)) {
+            throw new InvalidArgumentException('The timer mode must be one of: ' . implode(', ', self::SUPPORTED_MODES));
+        }
+    }
+
+    private function getExtensionsManagerService(): common_ext_ExtensionsManager
+    {
+        return $this->getServiceLocator()->get(common_ext_ExtensionsManager::SERVICE_ID);
+    }
+
+    private function getExtension(): common_ext_Extension
+    {
+        return $this->getExtensionsManagerService()->getExtensionById('taoQtiTest');
     }
 
     private function getTimerMode(): string
     {
-        /** @var \common_ext_Extension $taoQtiTestExtension */
-        $taoQtiTestExtension = $this->getExtensionsManagerService()->getExtensionById('taoQtiTest');
-        $config = $taoQtiTestExtension->getConfig('testRunner');
+        $config = $this->getExtension()->getConfig(self::CONFIG_FILE);
         return $config['timer']['target'];
     }
 
-    private function setTimerMode(string $mode): bool
+    private function setTimerMode(string $mode): void
     {
-        if (in_array($mode, self::SUPPORTED_MODES, true)) {
-
-            /** @var \common_ext_Extension $taoQtiTestExtension */
-            $taoQtiTestExtension = $this->getExtensionsManagerService()->getExtensionById('taoQtiTest');
-            $config = $taoQtiTestExtension->getConfig('testRunner');
-            $config['timer']['target'] = $mode;
-            $taoQtiTestExtension->setConfig('testRunner', $config);
-
-            return true;
-        }
-
-        return false;
+        $taoQtiTestExtension = $this->getExtension();
+        $taoQtiTestExtension->setConfig(
+            self::CONFIG_FILE,
+            array_replace_recursive($taoQtiTestExtension->getConfig(self::CONFIG_FILE), ['timer' => ['target' => $mode]])
+        );
     }
 }
