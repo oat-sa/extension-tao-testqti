720f2543d70da01e68c9e22387451573098929c9
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-02-03 11:50:32 +0100

    fix/ADF-933: Fix disconnect from delivery session (#2186)
    * fix: Pass an empty string to setNavigationContext() from Runner if scope request parameter is null
    * feat: Allow passing null for any parameter of setNavigationContext
    * feat: Require PHP >= 7.1 in composer.json, as we are introducing nullable parameters in a method signature
    * chore: Do not test if $ref is null in NavigationContextAwareTrait constructor
    * feat: Raise minimum required PHP version to 7.2
    
    Co-authored-by: Gabriel Felipe Soares <gabrielfs7@gmail.com>

---
+++ test/unit/model/Service/NavigationContextAwareTraitTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 (original work) Open Assessment Technologies SA.
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoQtiTest\test\unit\model\Service;
+
+use oat\generis\test\TestCase;
+use oat\taoQtiTest\model\Service\NavigationContextAwareTrait;
+
+class NavigationContextAwareTraitTest extends TestCase
+{
+    /** @var NavigationContextAwareTrait */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = $this->getObjectForTrait(
+            NavigationContextAwareTrait::class
+        );
+    }
+
+    /**
+     * @dataProvider setNavigationContextDataProvider
+     */
+    public function testSetNavigationContext(
+        string $expectedDirection,
+        string $expectedScope,
+        ?string $expectedRef,
+        ?string $direction,
+        ?string $scope,
+        ?string $ref
+    ): void {
+        $this->subject->setNavigationContext(
+            $direction,
+            $scope,
+            $ref
+        );
+
+        $this->assertSame($expectedDirection, $this->subject->getDirection());
+        $this->assertSame($expectedScope, $this->subject->getScope());
+        $this->assertSame($expectedRef, $this->subject->getRef());
+    }
+
+    public function setNavigationContextDataProvider(): array
+    {
+        return [
+            'All parameters set' => [
+                'expectedDirection' => 'direction',
+                'expectedScope' => 'scope',
+                'expectedRef' => 'ref',
+                'direction' => 'direction',
+                'scope' => 'scope',
+                'ref' => 'ref'
+            ],
+            'No direction set' => [
+                'expectedDirection' => '',
+                'expectedScope' => 'scope',
+                'expectedRef' => 'ref',
+
+                'direction' => null,
+                'scope' => 'scope',
+                'ref' => 'ref',
+            ],
+            'No scope set' => [
+                'expectedDirection' => 'direction',
+                'expectedScope' => '',
+                'expectedRef' => 'ref',
+
+                'direction' => 'direction',
+                'scope' => null,
+                'ref' => 'ref',
+            ],
+            'No ref set' => [
+                'expectedDirection' => 'direction',
+                'expectedScope' => 'scope',
+                'expectedRef' => null,
+
+                'direction' => 'direction',
+                'scope' => 'scope',
+                'ref' => null
+            ],
+        ];
+    }
+}
