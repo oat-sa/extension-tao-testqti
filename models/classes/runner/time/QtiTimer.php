5d9d8027ebbd16af382b796d2db3b80711129649
poyuk <kyril.poyu@taotesting.com>
2022-06-20 16:53:23 +0200

    feat: remove debug comment

--- models/classes/runner/time/QtiTimer.php
+++ models/classes/runner/time/QtiTimer.php
@@ -245,7 +245,6 @@ class QtiTimer implements Timer, ExtraTime, \JsonSerializable
         $clientTimeLine = $this->timeLine->filter($tags, TimePoint::TARGET_CLIENT);
         $clientRange = $clientTimeLine->getPoints();
         $clientRangeLength = count($clientRange);
-//        $logContext = [$this->storage->];
         if ($clientRangeLength) {
             $clientDuration = 0;
             try {
