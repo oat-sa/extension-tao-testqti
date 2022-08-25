a6f07afd501e990d2493382c4bf3580caa0215b5
uncleempty <ilya@taotesting.com>
2022-08-25 11:09:59 +0200

    chore: adding new functionality to null post processor

--- models/classes/render/CustomInteraction/PostProcessor/NullCustomInteractionPostProcessor.php
+++ models/classes/render/CustomInteraction/PostProcessor/NullCustomInteractionPostProcessor.php
@@ -22,12 +22,65 @@ declare(strict_types=1);
 
 namespace oat\taoQtiTest\models\render\CustomInteraction\PostProcessor;
 
+use oat\taoItems\model\render\ItemAssetsReplacement;
+use oat\taoItems\model\render\NoneItemReplacement;
 use oat\taoQtiTest\models\render\CustomInteraction\PostProcessor\Api\CustomInteractionPostProcessorInterface;
 
 class NullCustomInteractionPostProcessor implements CustomInteractionPostProcessorInterface
 {
+    public const INTERACTION_IDENTIFIER = 'null';
+
+    private const ASSET_REPLACER_PATTERN_OPTION = 'pattern';
+
+    /** @var ItemAssetsReplacement */
+    private $itemAssetsReplacement;
+
+    public function __construct(ItemAssetsReplacement $itemAssetsReplacement)
+    {
+        $this->itemAssetsReplacement = $itemAssetsReplacement;
+    }
+
     public function postProcess(array $element): array
     {
+        if (! ($this->itemAssetsReplacement instanceof NoneItemReplacement) ) {
+            $element = $this->iterateOverItemJson($element);
+        }
+
         return $element;
+    }
+
+    private function iterateOverItemJson(array $jsonArray): array
+    {
+        $url = ROOT_URL;
+        $parsedUrl = parse_url($url);
+        $rootUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+
+        foreach ($jsonArray as $key => $elem) {
+            if (is_array($elem)) {
+                if ($key === 'assets') {
+                    continue;
+                }
+                $elemA = $this->iterateOverItemJson($elem);
+                $jsonArray[$key] = $elemA;
+            } else {
+                if (false !== strpos($elem, $rootUrl)) {
+                    if (0 === strpos($elem, 'https')) {
+                        $elemS = $this->itemAssetsReplacement->postProcessAssets($elem);
+                        $jsonArray[$key] = $elemS;
+                    } else {
+                        $elemStripped = $elem;
+                        $pattern = $this->itemAssetsReplacement->getOption(self::ASSET_REPLACER_PATTERN_OPTION);
+                        preg_match_all($pattern, $elemStripped, $matches);
+                        foreach ($matches[1] as $match) {
+                            if (false !== strpos($match, $rootUrl)) {
+                                $elemStripped = str_replace($match, $this->itemAssetsReplacement->postProcessAssets($match), $elemStripped);
+                            }
+                        }
+                        $jsonArray[$key] = $elemStripped;
+                    }
+            }
+            }
+        }
+        return $jsonArray;
     }
 }
