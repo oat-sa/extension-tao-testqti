2d8c4afde56d885ef5edf2c0d9ff1d213d648e87
poyuk <kyril.poyu@taotesting.com>
2022-10-11 12:14:32 +0200

    fix: apply manual link for state manipulation tasks to the queue
    (cherry picked from commit 403cc090e1714f0ace21faa733b70302c70c2c3b)

--- scripts/install/SetupStateOffloadQueue.php
+++ scripts/install/SetupStateOffloadQueue.php
@@ -28,19 +28,23 @@ use oat\tao\model\taskQueue\Queue;
 use oat\tao\model\taskQueue\Queue\Broker\InMemoryQueueBroker;
 use oat\tao\model\taskQueue\QueueDispatcherInterface;
 use oat\tao\model\taskQueue\QueueInterface;
-use oat\taoQtiTest\models\classes\tasks\QtiStateOffload\AbstractQtiStateManipulationTask;
+use oat\taoQtiTest\models\classes\tasks\QtiStateOffload\StateBackupRemovalTask;
+use oat\taoQtiTest\models\classes\tasks\QtiStateOffload\StateOffloadTask;
+use oat\taoQtiTest\models\classes\tasks\QtiStateOffload\StateRemovalTask;
 
 class SetupStateOffloadQueue extends InstallAction
 {
+    public const QUEUE_NAME = 'state_management';
+
     public function __invoke($params): Report
     {
         $queueDispatcher = $this->getQueueDispatcher();
 
-        if ($queueDispatcher->hasQueue(AbstractQtiStateManipulationTask::QUEUE_NAME)) {
+        if ($queueDispatcher->hasQueue(self::QUEUE_NAME)) {
             return Report::createWarning(
                 sprintf(
                     'Queue %s already exists',
-                    AbstractQtiStateManipulationTask::QUEUE_NAME
+                    self::QUEUE_NAME
                 )
             );
         }
@@ -48,22 +52,28 @@ class SetupStateOffloadQueue extends InstallAction
         $newQueue->initialize();
 
         $queueDispatcher->addQueue($newQueue);
+        $queueDispatcher->linkTaskToQueue(StateBackupRemovalTask::class, self::QUEUE_NAME);
+        $queueDispatcher->linkTaskToQueue(StateOffloadTask::class, self::QUEUE_NAME);
+        $queueDispatcher->linkTaskToQueue(StateRemovalTask::class, self::QUEUE_NAME);
+
         $this->registerService(QueueDispatcherInterface::SERVICE_ID, $queueDispatcher);
 
         return Report::createSuccess(
             sprintf(
                 'Setup of %s proceed successfully',
-                AbstractQtiStateManipulationTask::QUEUE_NAME
+                self::QUEUE_NAME
             )
         );
     }
 
     private function buildNewQueue(): QueueInterface
     {
-        return $this->propagate(new Queue(
-            AbstractQtiStateManipulationTask::QUEUE_NAME,
-            new InMemoryQueueBroker()
-        ));
+        return $this->propagate(
+            new Queue(
+                self::QUEUE_NAME,
+                new InMemoryQueueBroker()
+            )
+        );
     }
 
     private function getQueueDispatcher(): QueueDispatcherInterface
