ff60d2f09d0eef56f26f329a90431025f08a8cba
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-06-02 17:26:16 +0200

    chore: Move author PHPDoc tags to classes

--- models/classes/runner/QtiRunnerService.php
+++ models/classes/runner/QtiRunnerService.php
@@ -15,9 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2023 (original work) Open Assessment Technologies SA ;
- *
- * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
+ * Copyright (c) 2016-2023 (original work) Open Assessment Technologies SA.
  */
 
 namespace oat\taoQtiTest\models\runner;
@@ -101,6 +99,7 @@ use taoResultServer_models_classes_TraceVariable;
  * QTI implementation service for the test runner
  *
  * @package oat\taoQtiTest\models
+ * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
  */
 class QtiRunnerService extends ConfigurableService implements PersistableRunnerServiceInterface, RunnerService
 {
