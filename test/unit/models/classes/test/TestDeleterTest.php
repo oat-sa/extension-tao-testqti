06a65c33d70080559431f7d2732ad355bc0b8e33
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-07-10 11:14:11 +0200

    test: Update unit test test

--- test/unit/models/classes/test/TestDeleterTest.php
+++ test/unit/models/classes/test/TestDeleterTest.php
@@ -22,153 +22,116 @@ declare(strict_types=1);
 
 namespace oat\taoQtiTest\models\test;
 
-use common_exception_Error;
-use common_ext_ExtensionException;
-//use oat\generis\test\TestCase;
-use oat\oatbox\service\ConfigurableService;
-use oat\oatbox\service\exception\InvalidService;
-use oat\oatbox\service\exception\InvalidServiceManagerException;
-use oat\oatbox\service\ServiceManager;
-use oat\tao\model\service\ApplicationService;
-use oat\taoQtiTest\models\test\Template\DefaultConfigurationRegistry;
-use qtism\data\AssessmentTest;
-use qtism\data\NavigationMode;
-use qtism\data\storage\xml\XmlStorageException;
-use qtism\data\SubmissionMode;
+use oat\generis\model\data\Ontology;
+use oat\tao\model\media\TaoMediaResolver;
+use oat\tao\model\resources\Service\ClassDeleter;
+use oat\taoMediaManager\model\MediaService;
+use oat\taoMediaManager\model\Specification\MediaClassSpecification;
+use oat\taoQtiItem\model\qti\parser\ElementReferencesExtractor;
+use oat\taoQtiItem\model\qti\Service;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
 use PHPUnit\Framework\TestCase;
-use RuntimeException;
-use SimpleXMLElement;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use taoItems_models_classes_ItemsService;
+use taoTests_models_classes_TestsService;
 
 class TestDeleterTest extends TestCase
 {
-    /** @var ServiceLocatorInterface */
-    private $serviceLocator;
+    /** @var (LoggerInterface&MockObject) */
+    private LoggerInterface $logger;
 
-    /** @var DefaultConfigurationRegistry */
-    private $xmlTemplateOptionRegistry;
+    /** @var Ontology&MockObject */
+    private Ontology $ontology;
 
-    /** @var array */
-    private $xmlTemplateRegistryOptions = [
-        'partIdPrefix'       => 'testPart',
-        'sectionIdPrefix'    => 'assessmentSection',
-        'sectionTitlePrefix' => 'Section',
-        'categories'         => [],
-        'navigationMode'     => 0,
-        'submissionMode'     => 0,
-        'maxAttempts'        => 0,
-    ];
+    /** @var Service&MockObject */
+    private Service $qtiItemService;
+
+    /** @var ClassDeleter&MockObject */
+    private ClassDeleter $classDeleter;
+
+    /** @var TaoMediaResolver&MockObject */
+    private TaoMediaResolver $taoMediaResolver;
+
+    /** @var taoTests_models_classes_TestsService&MockObject */
+    private taoTests_models_classes_TestsService $testsService;
+
+    /** @var taoItems_models_classes_ItemsService&MockObject */
+    private taoItems_models_classes_ItemsService $itemsService;
+
+    /** @var ElementReferencesExtractor&MockObject */
+    private ElementReferencesExtractor $elementReferencesExtractor;
+
+    /** @var (MediaClassSpecification&MockObject)|null */
+    private ?MediaClassSpecification $mediaClassSpecification;
+
+    /** @var (MediaService&MockObject)|null */
+    private ?MediaService $mediaService;
+
+    private ?TestDeleter $deleter;
+    private ?TestDeleter $assetAwareDeleter;
 
     protected function setUp(): void
     {
-        $this->xmlTemplateOptionRegistry = $this->createPartialMock(DefaultConfigurationRegistry::class, ['getMap']);
-        $this->xmlTemplateOptionRegistry
-            ->method('getMap')
-            ->willReturnCallback(
-                function (): array {
-                    return [DefaultConfigurationRegistry::ID => $this->xmlTemplateRegistryOptions];
-                }
+        if (!class_exists(taoItems_models_classes_ItemsService::class)) {
+            $this->markTestSkipped('This test needs taoItems to be present');
+        }
+
+        if (!class_exists(taoTests_models_classes_TestsService::class)) {
+            $this->markTestSkipped('This test needs taoTests to be present');
+        }
+
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->ontology = $this->createMock(Ontology::class);
+        $this->qtiItemService = $this->createMock(Service::class);
+        $this->classDeleter = $this->createMock(ClassDeleter::class);
+        $this->taoMediaResolver = $this->createMock(TaoMediaResolver::class);
+        $this->testsService = $this->createMock(taoTests_models_classes_TestsService::class);
+        $this->itemsService = $this->createMock(taoItems_models_classes_ItemsService::class);
+        $this->elementReferencesExtractor = $this->createMock(ElementReferencesExtractor::class);
+
+        if (class_exists(MediaClassSpecification::class)) {
+            $this->mediaClassSpecification = $this->createMock(MediaClassSpecification::class);
+        }
+
+        if (class_exists(MediaService::class)) {
+            $this->mediaService = $this->createMock(MediaService::class);
+        }
+
+        if (
+            $this->mediaService instanceof MediaService
+            && $this->mediaClassSpecification instanceof MediaClassSpecification
+        ) {
+            $this->assetAwareDeleter = new TestDeleter(
+                $this->logger,
+                $this->ontology,
+                $this->classDeleter,
+                $this->elementReferencesExtractor,
+                $this->qtiItemService,
+                $this->taoMediaResolver,
+                $this->itemsService,
+                $this->testsService,
+                $this->mediaClassSpecification,
+                $this->mediaService
             );
-
-        $appService = $this->createMock(ApplicationService::class);
-        $appService->method('getPlatformVersion')->willReturn('test_version');
-
-        $extension = new class () extends ConfigurableService implements TestExtensionInterface {
-            public function extend(AssessmentTest $test): void
-            {
-                $test->setTitle('changedTitle');
-            }
-        };
-
-        $badExtension = new class () extends ConfigurableService {
-        };
-
-        $this->serviceLocator = $this->createMock(ServiceManager::class);
-
-        $this->serviceLocator
-            ->method('get')
-            ->willReturnMap(
-                [
-                    [ApplicationService::SERVICE_ID, $appService],
-                    ['extension', $extension],
-                    ['badExtension', $badExtension],
-                    [DefaultConfigurationRegistry::class, $this->xmlTemplateOptionRegistry],
-                ]
+        } else {
+            $this->deleter = new TestDeleter(
+                $this->logger,
+                $this->ontology,
+                $this->classDeleter,
+                $this->elementReferencesExtractor,
+                $this->qtiItemService,
+                $this->taoMediaResolver,
+                $this->itemsService,
+                $this->testsService,
+                null,
+                null
             );
-
-        $this->xmlTemplateOptionRegistry->setServiceLocator($this->serviceLocator);
+        }
     }
 
-    /**
-     * @dataProvider provideData
-     *
-     * @param array  $options
-     * @param string $expectedTestPartId
-     * @param string $expectedNavigationMode
-     * @param string $expectedSubmissionMode
-     * @param string $expectedSectionTitle
-     * @param string $expectedSectionId
-     * @param int    $expectedMaxAttempts
-     *
-     * @throws InvalidService
-     * @throws InvalidServiceManagerException
-     * @throws XmlStorageException
-     * @throws common_exception_Error
-     * @throws common_ext_ExtensionException
-     */
-    public function testBuild(
-        array $options,
-        string $expectedTestPartId,
-        string $expectedNavigationMode,
-        string $expectedSubmissionMode,
-        string $expectedSectionTitle,
-        string $expectedSectionId,
-        int $expectedMaxAttempts
+    public function testSomething(
+
     ): void {
-        $this->xmlTemplateRegistryOptions = $options;
-
-        $builder = $this->createBuilder();
-
-        $xml = $builder->create('testId', 'testLabel');
-
-        static::assertIsString($xml);
-
-        $simpleXml = new SimpleXMLElement($xml);
-        static::assertSame('testId', (string)$simpleXml->attributes()['identifier']);
-        static::assertSame('testLabel', (string)$simpleXml->attributes()['title']);
-
-        /** @noinspection PhpUndefinedFieldInspection */
-        $testPartAttributes = $simpleXml->testPart->attributes();
-
-        static::assertSame(
-            $expectedTestPartId,
-            (string)$testPartAttributes['identifier']
-        );
-        static::assertSame(
-            $expectedNavigationMode,
-            (string)$testPartAttributes['navigationMode']
-        );
-        static::assertSame(
-            $expectedSubmissionMode,
-            (string)$testPartAttributes['submissionMode']
-        );
-
-        $assessmentSectionAttributes = $simpleXml->testPart->assessmentSection->attributes();
-        static::assertSame(
-            $expectedSectionId,
-            (string)$assessmentSectionAttributes['identifier']
-        );
-        static::assertSame(
-            $expectedSectionTitle,
-            (string)$assessmentSectionAttributes['title']
-        );
-
-        /** @noinspection PhpUndefinedFieldInspection */
-        $itemSessionControl = $simpleXml->testPart->itemSessionControl->attributes();
-
-        static::assertSame(
-            $expectedMaxAttempts,
-            (int)$itemSessionControl['maxAttempts']
-        );
+        $this->markTestIncomplete('Not implemented');
     }
 }
