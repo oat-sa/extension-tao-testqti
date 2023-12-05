dd94d78df7505d1d195fa7942b7f574afe3e08ef
Karol Stelmaczonek <karol.stelmaczonek@taotesting.com>
2023-12-05 09:11:59 +0100

    feature: add new event ResultTestVariablesAfterTransmissionEvent

---
+++ models/classes/event/ResultTestVariablesAfterTransmissionEvent.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2023 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+
+namespace oat\taoQtiTest\models\event;
+
+use oat\oatbox\event\Event;
+
+class ResultTestVariablesAfterTransmissionEvent implements Event
+{
+    private string $deliveryExecutionId;
+
+    private array $variables;
+
+    public function __construct(
+        string $deliveryExecutionId,
+        array $variables
+    ) {
+        $this->deliveryExecutionId = $deliveryExecutionId;
+        $this->variables = $variables;
+    }
+
+    public function getName(): string
+    {
+        return self::class;
+    }
+
+    public function getDeliveryExecutionId(): string
+    {
+        return $this->deliveryExecutionId;
+    }
+
+    public function getVariables(): array
+    {
+        return $this->variables;
+    }
+}
