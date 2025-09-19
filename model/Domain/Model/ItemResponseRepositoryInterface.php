df18f0f84722e7aa73a931024789cf21952401f9
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2025-09-19 12:08:42 +0200

    fix: allow empty responses on timeout

--- model/Domain/Model/ItemResponseRepositoryInterface.php
+++ model/Domain/Model/ItemResponseRepositoryInterface.php
@@ -8,16 +8,14 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
- *
- * @author Ricardo Quintanilha <ricardo.quintanilha@taotesting.com>
+ * Copyright (c) 2021-2025 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
@@ -28,5 +26,9 @@ use oat\taoQtiTest\models\runner\RunnerServiceContext;
 
 interface ItemResponseRepositoryInterface
 {
-    public function save(ItemResponse $itemResponse, RunnerServiceContext $serviceContext): void;
+    public function save(
+        ItemResponse $itemResponse,
+        RunnerServiceContext $serviceContext,
+        bool $allowEmptyResponse = false
+    ): void;
 }
