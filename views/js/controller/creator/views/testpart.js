022ac49f3a92d731455e72b7a4b6c72ec0b7c652
Yaroslav Kikot <13ccdd@gmail.com>
2026-01-27 11:14:05 +0100

    chore: update license headers

--- views/js/controller/creator/views/testpart.js
+++ views/js/controller/creator/views/testpart.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2014-2025 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2014-2026 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 /**
  * @author Bertrand Chevrier <bertrand@taotesting.com>
